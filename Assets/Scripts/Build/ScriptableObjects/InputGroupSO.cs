7021d4083073093656e86e717b3423a541448364
codaishin <codaishin@gmail.com>
2020-12-29 15:56:21 +0100

    InputGroupSO: Added menu name

--- Assets/Scripts/Build/ScriptableObjects/InputGroupSO.cs
+++ Assets/Scripts/Build/ScriptableObjects/InputGroupSO.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "ScriptableObjects/InputGroup")]
 public class InputGroupSO : ScriptableObject
 {
 	public BaseInputSO inputSO;
