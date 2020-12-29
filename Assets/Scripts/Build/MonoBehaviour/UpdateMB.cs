47b53ec0229f3e40f8823427bf150c38078c61e9
codaishin <codaishin@gmail.com>
2020-12-29 16:04:43 +0100

    UpdateMB: Implemented

--- Assets/Scripts/Build/MonoBehaviour/UpdateMB.cs
+++ Assets/Scripts/Build/MonoBehaviour/UpdateMB.cs
@@ -1,8 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class UpdateMB : MonoBehaviour
 {
+	public UnityEvent onUpdate;
 
+	private void Start()
+	{
+		if (this.onUpdate == null) {
+			this.onUpdate = new UnityEvent();
+		}
+	}
+
+	private void Update() => this.onUpdate.Invoke();
 }
