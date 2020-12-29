06f74b3fe0b3514df158a63f441dd0ff11504214
codaishin <codaishin@gmail.com>
2020-12-29 13:39:57 +0100

    InputSO: Implemented

--- Assets/Scripts/Build/ScriptableObjects/InputSO.cs
+++ Assets/Scripts/Build/ScriptableObjects/InputSO.cs
@@ -1,8 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class InputSO : ScriptableObject
-{
+public enum KeyState { Hold = default, Down, Up }
 
+public abstract class BaseInputSO : ScriptableObject
+{
+	protected abstract bool Get(in KeyCode code);
+	protected abstract bool GetDown(in KeyCode code);
+	protected abstract bool GetUp(in KeyCode code);
+
+	public bool GetKey(in KeyCode code, in KeyState state) => state switch {
+			KeyState.Up => this.GetUp(code),
+			KeyState.Down => this.GetDown(code),
+			KeyState.Hold => this.Get(code),
+			_ => throw new ArgumentException($"KeyState \"{state}\" not recognised"),
+		};
+}
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Input")]
+public class InputSO: BaseInputSO
+{
+	protected override bool Get(in KeyCode code) => Input.GetKey(code);
+	protected override bool GetDown(in KeyCode code) => Input.GetKeyDown(code);
+	protected override bool GetUp(in KeyCode code) => Input.GetKeyUp(code);
 }
--- Assets/Scripts/Tests/ScriptableObjects/InputSOTests.cs
+++ Assets/Scripts/Tests/ScriptableObjects/InputSOTests.cs
@@ -6,5 +6,106 @@ using UnityEngine.TestTools;
 
 public class InputSOTests : TestCollection
 {
+	private class MockInputSO : BaseInputSO
+	{
+		public Dictionary<KeyCode, KeyState> keyStates =
+			new Dictionary<KeyCode, KeyState>();
 
+		protected override bool Get(in KeyCode code) =>
+			this.keyStates.TryGetValue(code, out KeyState state) &&
+			state == KeyState.Hold;
+		protected override bool GetDown(in KeyCode code) =>
+			this.keyStates.TryGetValue(code, out KeyState state) &&
+			state == KeyState.Down;
+		protected override bool GetUp(in KeyCode code) =>
+			this.keyStates.TryGetValue(code, out KeyState state) &&
+			state == KeyState.Up;
+	}
+
+	[Test]
+	public void HoldSpace()
+	{
+		var key = KeyCode.Space;
+		var mode = KeyState.Hold;
+		var inputSO = ScriptableObject.CreateInstance<MockInputSO>();
+		inputSO.keyStates[key] = mode;
+
+		Assert.True(inputSO.GetKey(key, mode));
+	}
+
+	[Test]
+	public void HoldSpaceFalse()
+	{
+		var key = KeyCode.Space;
+		var mode = KeyState.Hold;
+		var inputSO = ScriptableObject.CreateInstance<MockInputSO>();
+
+		Assert.False(inputSO.GetKey(key, mode));
+	}
+
+	[Test]
+	public void DownK()
+	{
+		var key = KeyCode.K;
+		var mode = KeyState.Down;
+		var inputSO = ScriptableObject.CreateInstance<MockInputSO>();
+		inputSO.keyStates[key] = mode;
+
+		Assert.True(inputSO.GetKey(key, mode));
+	}
+
+	[Test]
+	public void DownKFalse()
+	{
+		var key = KeyCode.K;
+		var mode = KeyState.Down;
+		var inputSO = ScriptableObject.CreateInstance<MockInputSO>();
+
+		Assert.False(inputSO.GetKey(key, mode));
+	}
+
+	[Test]
+	public void UpE()
+	{
+		var key = KeyCode.E;
+		var mode = KeyState.Up;
+		var inputSO = ScriptableObject.CreateInstance<MockInputSO>();
+		inputSO.keyStates[key] = mode;
+
+		Assert.True(inputSO.GetKey(key, mode));
+	}
+
+	[Test]
+	public void UpEFalse()
+	{
+		var key = KeyCode.E;
+		var mode = KeyState.Up;
+		var inputSO = ScriptableObject.CreateInstance<MockInputSO>();
+
+		Assert.False(inputSO.GetKey(key, mode));
+	}
+
+	[Test]
+	public void OddStateWThrows()
+	{
+		var key = KeyCode.W;
+		var mode = (KeyState)(-1);
+		var inputSO = ScriptableObject.CreateInstance<MockInputSO>();
+
+		Assert.Throws<System.ArgumentException>(() => inputSO.GetKey(key, mode));
+	}
+
+	[Test]
+	public void OddStateWThrowsMessage()
+	{
+		var key = KeyCode.W;
+		var mode = (KeyState)(-1);
+		var inputSO = ScriptableObject.CreateInstance<MockInputSO>();
+
+		try {
+			inputSO.GetKey(key, mode);
+		} catch (System.ArgumentException e) {
+			Assert.AreEqual("KeyState \"-1\" not recognised", e.Message);
+		}
+	}
 }
