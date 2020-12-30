a9b99b410279b60d757000b5ce652b6bd0b01ee0
codaishin <codaishin@gmail.com>
2020-12-30 12:03:40 +0100

    RayCastHitMB: basic onHitGameObject

--- Assets/Scripts/Build/MonoBehaviour/RayCastHitMB.cs
+++ Assets/Scripts/Build/MonoBehaviour/RayCastHitMB.cs
@@ -1,13 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public abstract class BaseRayProviderMB : MonoBehaviour
 {
 	public abstract Ray Ray { get; }
 }
 
+[Serializable]
+public class GameObjectEvent : UnityEvent<GameObject> {}
+
 public class RayCastHitMB : MonoBehaviour
 {
+	public BaseRayProviderMB rayProviderMB;
+	public GameObjectEvent onHitGameObject;
 
+	private void Start()
+	{
+		if (this.onHitGameObject == null) {
+			this.onHitGameObject = new GameObjectEvent();
+		}
+	}
+
+	public void TryHit()
+	{
+		if (Physics.Raycast(this.rayProviderMB.Ray, out RaycastHit hit, float.MaxValue)) {
+			this.onHitGameObject.Invoke(hit.transform.gameObject);
+		}
+	}
 }
--- Assets/Scripts/Tests/MonoBehaviour/RayCastHitMBTests.cs
+++ Assets/Scripts/Tests/MonoBehaviour/RayCastHitMBTests.cs
@@ -6,5 +6,50 @@ using UnityEngine.TestTools;
 
 public class RayCastHitMBTests : TestCollection
 {
+	private class MockRayMB : BaseRayProviderMB
+	{
+		public Ray ray;
 
+		public override Ray Ray => this.ray;
+	}
+
+	[UnityTest]
+	public IEnumerator OnHitGameObjectInitAfterStart()
+	{
+		var rayCastHitMB = new GameObject("hitter").AddComponent<RayCastHitMB>();
+
+		yield return new WaitForEndOfFrame();
+
+		Assert.NotNull(rayCastHitMB.onHitGameObject);
+	}
+
+	[UnityTest]
+	public IEnumerator OnHitGameObjectDefaultNull()
+	{
+		var rayCastHitMB = new GameObject("hitter").AddComponent<RayCastHitMB>();
+
+		Assert.Null(rayCastHitMB.onHitGameObject);
+
+		yield break;
+	}
+
+	[UnityTest]
+	public IEnumerator OnHitGameObject()
+	{
+		var hit = null as GameObject;
+		var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		var rayCastHitMB = new GameObject("hitter").AddComponent<RayCastHitMB>();
+		var rayProviderMB = new GameObject("ray").AddComponent<MockRayMB>();
+
+		sphere.transform.position = Vector3.up;
+		rayProviderMB.ray = new Ray(Vector3.up * 2, Vector3.down);
+		rayCastHitMB.rayProviderMB = rayProviderMB;
+
+		yield return new WaitForEndOfFrame();
+
+		rayCastHitMB.onHitGameObject.AddListener(o => hit = o);
+		rayCastHitMB.TryHit();
+
+		Assert.AreSame(sphere, hit);
+	}
 }
