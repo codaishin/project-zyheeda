d169b5bd650cb89f25643eac5cb1dd8da62e667c
codaishin <codaishin@gmail.com>
2020-12-30 16:33:16 +0100

    Reference: Implemented

--- Assets/Scripts/Tests/SerializedTypes/ReferenceTests.cs
+++ Assets/Scripts/Tests/SerializedTypes/ReferenceTests.cs
@@ -6,5 +6,47 @@ using UnityEngine.TestTools;
 
 public class ReferenceTests : TestCollection
 {
+	[Test]
+	public void ExposeGameObject()
+	{
+		var obj = new GameObject("obj");
+		var reference = new Reference(obj);
 
+		Assert.AreSame(obj, reference.GameObject);
+	}
+
+	[Test]
+	public void ExposeReferenceGameObject()
+	{
+		var obj = new GameObject("obj");
+		var referenceSO = ScriptableObject.CreateInstance<ReferenceSO>();
+		var reference = new Reference(referenceSO);
+
+		referenceSO.GameObject = obj;
+		Assert.AreSame(obj, reference.GameObject);
+	}
+
+	[Test]
+	public void ExceptionWhenGameObjectAndReferenceSOSet()
+	{
+		var obj = new GameObject("obj");
+		var referenceSO = ScriptableObject.CreateInstance<ReferenceSO>();
+		var reference = new Reference(obj, referenceSO);
+
+		Assert.Throws<System.ArgumentException>(() => _ = reference.GameObject);
+	}
+
+	[Test]
+	public void ExceptionWhenGameObjectAndReferenceSOSetMessage()
+	{
+		var obj = new GameObject("obj");
+		var referenceSO = ScriptableObject.CreateInstance<ReferenceSO>();
+		var reference = new Reference(obj, referenceSO);
+
+		try {
+			_ = reference.GameObject;
+		} catch (System.ArgumentException e) {
+			Assert.AreEqual("\"gameObject\" and \"referenceSO\" are both set", e.Message);
+		}
+	}
 }
