325788b792595a2e64b420769c9f004f1c216240
codaishin <codaishin@gmail.com>
2020-12-30 16:05:47 +0100

    ReferenceSetterMB: SetReference on Start

--- Assets/Scripts/Build/MonoBehaviour/ReferenceSetterMB.cs
+++ Assets/Scripts/Build/MonoBehaviour/ReferenceSetterMB.cs
@@ -1,6 +1,22 @@
+using System;
 using UnityEngine;
 
 public class ReferenceSetterMB : MonoBehaviour
 {
+	public ReferenceSO referenceSO;
 
+	private void Start() => this.SetReference();
+
+	public void SetReference()
+	{
+		if (this.referenceSO.GameObject) {
+			if (this.referenceSO.GameObject != this.gameObject) {
+				throw new ArgumentException(
+					$"\"{this.referenceSO.name}\" already set to \"{this.referenceSO.GameObject.name}\""
+				);
+			}
+		} else {
+			this.referenceSO.GameObject = this.gameObject;
+		}
+	}
 }
--- Assets/Scripts/Tests/MonoBehaviour/ReferenceSetterMBTests.cs
+++ Assets/Scripts/Tests/MonoBehaviour/ReferenceSetterMBTests.cs
@@ -1,10 +1,79 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-public class ReferenceSetterMBTests
+public class ReferenceSetterMBTests : TestCollection
 {
+	[Test]
+	public void Set()
+	{
+		var obj = new GameObject("obj");
+		var setterMB = obj.AddComponent<ReferenceSetterMB>();
+		var referenceSO = ScriptableObject.CreateInstance<ReferenceSO>();
 
+		setterMB.referenceSO = referenceSO;
+		setterMB.SetReference();
+
+		Assert.AreSame(obj, referenceSO.GameObject);
+	}
+
+	[Test]
+	public void ErrorWhenGameObjectAlreadySet()
+	{
+		var obj = new GameObject("obj");
+		var setterMB = obj.AddComponent<ReferenceSetterMB>();
+		var referenceSO = ScriptableObject.CreateInstance<ReferenceSO>();
+
+		setterMB.referenceSO = referenceSO;
+		referenceSO.GameObject = new GameObject("other");
+
+		Assert.Throws<ArgumentException>(() => setterMB.SetReference());
+	}
+
+	[Test]
+	public void ErrorWhenGameObjectAlreadySetMessage()
+	{
+		var obj = new GameObject("obj");
+		var setterMB = obj.AddComponent<ReferenceSetterMB>();
+		var referenceSO = ScriptableObject.CreateInstance<ReferenceSO>();
+
+		setterMB.referenceSO = referenceSO;
+		referenceSO.GameObject = new GameObject("other");
+		referenceSO.name = "my object";
+
+		try {
+			setterMB.SetReference();
+		} catch (ArgumentException e) {
+			Assert.AreEqual("\"my object\" already set to \"other\"", e.Message);
+		}
+	}
+
+	[Test]
+	public void NoErrorWhenAlreadySetToSelf()
+	{
+		var obj = new GameObject("obj");
+		var setterMB = obj.AddComponent<ReferenceSetterMB>();
+		var referenceSO = ScriptableObject.CreateInstance<ReferenceSO>();
+
+		setterMB.referenceSO = referenceSO;
+		referenceSO.GameObject = obj;
+
+		Assert.DoesNotThrow(() => setterMB.SetReference());
+	}
+
+	[UnityTest]
+	public IEnumerator SetOnStart()
+	{
+		var obj = new GameObject("obj");
+		var setterMB = obj.AddComponent<ReferenceSetterMB>();
+		var referenceSO = ScriptableObject.CreateInstance<ReferenceSO>();
+
+		setterMB.referenceSO = referenceSO;
+
+		yield return new WaitForEndOfFrame();
+
+		Assert.AreSame(obj, referenceSO.GameObject);
+	}
 }
