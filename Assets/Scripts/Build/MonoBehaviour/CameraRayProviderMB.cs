4b1de4e4c5eb6cc712ad9fe9a67b375df5abe9ea
codaishin <codaishin@gmail.com>
2020-12-30 14:43:34 +0100

    CameraRayProviderMB: cleanup

--- Assets/Scripts/Build/MonoBehaviour/CameraRayProviderMB.cs
+++ Assets/Scripts/Build/MonoBehaviour/CameraRayProviderMB.cs
@@ -1,12 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
 public class CameraRayProviderMB : BaseRayProviderMB
 {
-	public Camera Camera { get; private set; }
 	public BaseMouseSO mouseSO;
+
+	public Camera Camera { get; private set; }
 	public override Ray Ray => this.Camera.ScreenPointToRay(this.mouseSO.Position);
 
 	private void Awake() => this.Camera = this.GetComponent<Camera>();
