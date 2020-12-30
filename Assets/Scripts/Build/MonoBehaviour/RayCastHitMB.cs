fc85e0892776c641132a1836ba09322eee1a94a7
codaishin <codaishin@gmail.com>
2020-12-30 12:19:02 +0100

    RayCastHitMB: onHitVector3 base implementation

--- Assets/Scripts/Build/MonoBehaviour/RayCastHitMB.cs
+++ Assets/Scripts/Build/MonoBehaviour/RayCastHitMB.cs
@@ -10,15 +10,22 @@ public abstract class BaseRayProviderMB : MonoBehaviour
 [Serializable]
 public class GameObjectEvent : UnityEvent<GameObject> {}
 
+[Serializable]
+public class Vector3Event : UnityEvent<Vector3> {}
+
 public class RayCastHitMB : MonoBehaviour
 {
 	public BaseRayProviderMB rayProviderMB;
 	public GameObjectEvent onHitGameObject;
+	public Vector3Event onHitVector3;
 
 	private void Start()
 	{
 		if (this.onHitGameObject == null) {
 			this.onHitGameObject = new GameObjectEvent();
+		}
+		if (this.onHitVector3 == null) {
+			this.onHitVector3 = new Vector3Event();
 		}
 	}
 
@@ -26,6 +33,7 @@ public class RayCastHitMB : MonoBehaviour
 	{
 		if (Physics.Raycast(this.rayProviderMB.Ray, out RaycastHit hit, float.MaxValue)) {
 			this.onHitGameObject.Invoke(hit.transform.gameObject);
+			this.onHitVector3.Invoke(hit.point);
 		}
 	}
 }
--- Assets/Scripts/Tests/MonoBehaviour/RayCastHitMBTests.cs
+++ Assets/Scripts/Tests/MonoBehaviour/RayCastHitMBTests.cs
@@ -46,10 +46,52 @@ public class RayCastHitMBTests : TestCollection
 		rayCastHitMB.rayProviderMB = rayProviderMB;
 
 		yield return new WaitForEndOfFrame();
+		yield return new WaitForEndOfFrame();
 
 		rayCastHitMB.onHitGameObject.AddListener(o => hit = o);
 		rayCastHitMB.TryHit();
 
 		Assert.AreSame(sphere, hit);
+	}
+
+	[UnityTest]
+	public IEnumerator OnHitVector3InitAfterStart()
+	{
+		var rayCastHitMB = new GameObject("hitter").AddComponent<RayCastHitMB>();
+
+		yield return new WaitForEndOfFrame();
+
+		Assert.NotNull(rayCastHitMB.onHitVector3);
+	}
+
+	[UnityTest]
+	public IEnumerator OnHitVector3DefaultNull()
+	{
+		var rayCastHitMB = new GameObject("hitter").AddComponent<RayCastHitMB>();
+
+		Assert.Null(rayCastHitMB.onHitVector3);
+
+		yield break;
+	}
+
+	[UnityTest]
+	public IEnumerator OnHitVector3Object()
+	{
+		var hit = Vector3.zero;
+		var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		var rayCastHitMB = new GameObject("hitter").AddComponent<RayCastHitMB>();
+		var rayProviderMB = new GameObject("ray").AddComponent<MockRayMB>();
+
+		sphere.transform.position = Vector3.up;
+		rayProviderMB.ray = new Ray(Vector3.up * 2, Vector3.down);
+		rayCastHitMB.rayProviderMB = rayProviderMB;
+
+		yield return new WaitForEndOfFrame();
+		yield return new WaitForEndOfFrame();
+
+		rayCastHitMB.onHitVector3.AddListener(v => hit = v);
+		rayCastHitMB.TryHit();
+
+		Assert.AreEqual(new Vector3(0, 1.5f, 0), hit);
 	}
 }
