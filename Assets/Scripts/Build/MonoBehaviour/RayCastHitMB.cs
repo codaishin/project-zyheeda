7dec1d34fd5fbfe8398a38989cdb47121fb10a69
codaishin <codaishin@gmail.com>
2020-12-30 11:44:52 +0100

    RayCastHitMB: Added

---
+++ Assets/Scripts/Build/MonoBehaviour/RayCastHitMB.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseRayProviderMB : MonoBehaviour
+{
+	public abstract Ray Ray { get; }
+}
+
+public class RayCastHitMB : MonoBehaviour
+{
+
+}
