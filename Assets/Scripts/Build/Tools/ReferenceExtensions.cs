551063cb317476700895e79ce3349789a17fd1a9
codaishin <codaishin@gmail.com>
2020-12-30 17:03:11 +0100

    ReferenceExtension: Implemented Get

--- Assets/Scripts/Build/Tools/ReferenceExtensions.cs
+++ Assets/Scripts/Build/Tools/ReferenceExtensions.cs
@@ -1,8 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public static class ReferenceExtensions
 {
-
+	public static C Get<C>(this Reference reference, ref C buffer)
+		where C: Component
+	{
+		if (!buffer) {
+			buffer = reference.GameObject.GetComponent<C>();
+		}
+		return buffer;
+	}
 }
--- Assets/Scripts/Tests/Tools/ReferenceExtensionsTests.cs
+++ Assets/Scripts/Tests/Tools/ReferenceExtensionsTests.cs
@@ -6,5 +6,35 @@ using UnityEngine.TestTools;
 
 public class ReferenceExtensionsTests : TestCollection
 {
+	[Test]
+	public void Get()
+	{
+		var collider = new GameObject("obj").AddComponent<BoxCollider>();
+		var reference = new Reference(collider.gameObject);
+		var buffer = null as BoxCollider;
 
+		Assert.AreSame(collider, reference.Get(ref buffer));
+	}
+
+	[Test]
+	public void GetSetsBuffer()
+	{
+		var collider = new GameObject("obj").AddComponent<BoxCollider>();
+		var reference = new Reference(collider.gameObject);
+		var buffer = null as BoxCollider;
+
+		reference.Get(ref buffer);
+
+		Assert.AreSame(collider, buffer);
+	}
+
+	[Test]
+	public void GetReturnsBufferWhenSet()
+	{
+		var collider = new GameObject("obj").AddComponent<BoxCollider>();
+		var reference = new Reference(collider.gameObject);
+		var buffer = new GameObject("buffer override").AddComponent<BoxCollider>();
+
+		Assert.AreSame(buffer, reference.Get(ref buffer));
+	}
 }
