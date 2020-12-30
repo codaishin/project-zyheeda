a7e68b2ece43ac042b4e7ffdb3214b9c7095ddad
codaishin <codaishin@gmail.com>
2020-12-30 14:29:01 +0100

    MouseSO: Created menu item entry

--- Assets/Scripts/Build/ScriptableObjects/MouseSO.cs
+++ Assets/Scripts/Build/ScriptableObjects/MouseSO.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "ScriptableObjects/Mouse")]
 public class MouseSO : BaseMouseSO
 {
 	public override Vector3 Position => Input.mousePosition;
