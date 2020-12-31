4d8f795cde25c5e22ca13aa68a3177c512f9c505
codaishin <codaishin@gmail.com>
2020-12-31 14:02:12 +0100

    PlayStateReaderMB: implemented onStateEnter

--- Assets/Scripts/Build/MonoBehaviour/PlayStateReaderMB.cs
+++ Assets/Scripts/Build/MonoBehaviour/PlayStateReaderMB.cs
@@ -1,8 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayStateReaderMB : MonoBehaviour
 {
+	public PlayStateSwitchSO stateSwitch;
+	public PlayState state;
 
+	public UnityEvent onStateEnter;
+
+	private void Start()
+	{
+		if (this.onStateEnter == null) {
+			this.onStateEnter = new UnityEvent();
+		}
+		this.stateSwitch.OnStateChange += this.Apply;
+	}
+
+	private void OnDestroy()
+	{
+		this.stateSwitch.OnStateChange -= this.Apply;
+	}
+
+	private void Apply(PlayState state)
+	{
+		if (state == this.state) {
+			this.onStateEnter.Invoke();
+		}
+	}
 }
--- Assets/Scripts/Tests/MonoBehaviour/PlayStateReaderMBTests.cs
+++ Assets/Scripts/Tests/MonoBehaviour/PlayStateReaderMBTests.cs
@@ -6,5 +6,67 @@ using UnityEngine.TestTools;
 
 public class PlayStateReaderMBTests : TestCollection
 {
+	[UnityTest]
+	public IEnumerator OnStateEnterNotNullAfterStart()
+	{
+		var reader = new GameObject("reader").AddComponent<PlayStateReaderMB>();
+		var stateSwitch = ScriptableObject.CreateInstance<PlayStateSwitchSO>();
+		reader.stateSwitch = stateSwitch;
 
+		yield return new WaitForEndOfFrame();
+
+		Assert.NotNull(reader.onStateEnter);
+	}
+
+	[UnityTest]
+	public IEnumerator OnStateEnterDefaultNull()
+	{
+		var reader = new GameObject("reader").AddComponent<PlayStateReaderMB>();
+		var stateSwitch = ScriptableObject.CreateInstance<PlayStateSwitchSO>();
+		reader.stateSwitch = stateSwitch;
+
+		Assert.Null(reader.onStateEnter);
+
+		yield break;
+	}
+
+	[UnityTest]
+	public IEnumerator OnStateEnter()
+	{
+		var called = 0;
+		var reader = new GameObject("reader").AddComponent<PlayStateReaderMB>();
+		var stateSwitch = ScriptableObject.CreateInstance<PlayStateSwitchSO>();
+
+		reader.stateSwitch = stateSwitch;
+		reader.state = PlayState.Paused;
+
+		yield return new WaitForEndOfFrame();
+
+		reader.onStateEnter.AddListener(() => ++called);
+		stateSwitch.State = PlayState.Paused;
+
+		Assert.AreEqual(1, called);
+	}
+
+	[UnityTest]
+	public IEnumerator OnStateEnterUnsubscribeWhenDestroyed()
+	{
+		var called = 0;
+		var reader = new GameObject("reader").AddComponent<PlayStateReaderMB>();
+		var stateSwitch = ScriptableObject.CreateInstance<PlayStateSwitchSO>();
+
+		reader.stateSwitch = stateSwitch;
+		reader.state = PlayState.Paused;
+
+		yield return new WaitForEndOfFrame();
+
+		reader.onStateEnter.AddListener(() => ++called);
+		Object.Destroy(reader);
+
+		yield return new WaitForEndOfFrame();
+
+		stateSwitch.State = PlayState.Paused;
+
+		Assert.AreEqual(0, called);
+	}
 }
