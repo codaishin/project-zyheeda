9deff1ebd5027e8063f591ed24103ef68b0f3607
codaishin <codaishin@gmail.com>
2020-12-31 17:15:26 +0100

    IPauseableExtensions: Refactor

--- Assets/Scripts/Build/Tools/IPausableExtensions.cs
+++ Assets/Scripts/Build/Tools/IPausableExtensions.cs
@@ -2,18 +2,29 @@ using System.Collections;
 
 public static class IPausableExtensions
 {
+	private static
+	bool PausedOrNextOf<TPauseYield>(this IPausable<TPauseYield> pausable,
+	                                 in IEnumerator enumerator,
+	                                 out object value)
+	{
+		if (pausable.Paused) {
+			value = pausable.Pause;
+			return true;
+		}
+		if (enumerator.MoveNext()) {
+			value = enumerator.Current;
+			return true;
+		}
+		value = default;
+		return false;
+	}
+
 	public static
 	IEnumerator Manage<TPauseYield>(this IPausable<TPauseYield> pausable,
 	                                IEnumerator enumerator)
 	{
-		while (true) {
-			if (pausable.Paused) {
-				yield return pausable.Pause;
-			} else if (enumerator.MoveNext()) {
-				yield return enumerator.Current;
-			} else {
-				yield break;
-			}
+		while (pausable.PausedOrNextOf(enumerator, out object value)) {
+			yield return value;
 		};
 	}
 }
