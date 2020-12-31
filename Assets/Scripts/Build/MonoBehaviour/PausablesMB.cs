d490b8cd88c9890dadb7531a8c4e9684deb84b2c
codaishin <codaishin@gmail.com>
2020-12-31 17:33:21 +0100

    PasuableMB: Implemented

--- Assets/Scripts/Build/MonoBehaviour/PausablesMB.cs
+++ Assets/Scripts/Build/MonoBehaviour/PausablesMB.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class PausablesMB : MonoBehaviour
 {
+	private IPausable[] pausables;
 
+	private void Start()
+	{
+		this.pausables = this.GetComponentsInChildren<IPausable>();
+	}
+
+	public void PauseChildren(bool value) =>
+		this.pausables.ForEach(p => p.Paused = value);
 }
--- Assets/Scripts/Tests/MonoBehaviour/PausablesMBTests.cs
+++ Assets/Scripts/Tests/MonoBehaviour/PausablesMBTests.cs
@@ -6,5 +6,25 @@ using UnityEngine.TestTools;
 
 public class PausablesMBTests : TestCollection
 {
+	private class MockPausableMB : MonoBehaviour, IPausable
+	{
+		public bool Paused { get; set; }
+	}
 
+	[UnityTest]
+	public IEnumerator PauseChildren()
+	{
+		var parent = new GameObject("pausables").AddComponent<PausablesMB>();
+		var childA = new GameObject("pausableA").AddComponent<MockPausableMB>();
+		var childB = new GameObject("pausableB").AddComponent<MockPausableMB>();
+
+		childA.transform.parent = parent.transform;
+		childB.transform.parent = parent.transform;
+
+		yield return new WaitForEndOfFrame();
+
+		parent.PauseChildren(true);
+
+		Assert.AreEqual((true, true), (childA.Paused, childB.Paused));
+	}
 }
