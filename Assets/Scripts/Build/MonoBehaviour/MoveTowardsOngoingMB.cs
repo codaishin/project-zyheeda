1344ee1f14ace7392959cea9d651e17aaa8c5134
codaishin <codaishin@gmail.com>
2020-12-31 11:32:17 +0100

    MoveTowardsOngoingMB: Instant move with speed

--- Assets/Scripts/Build/MonoBehaviour/MoveTowardsOngoingMB.cs
+++ Assets/Scripts/Build/MonoBehaviour/MoveTowardsOngoingMB.cs
@@ -7,11 +7,20 @@ public class MoveTowardsOngoingMB : MonoBehaviour
 	private Transform agentTransform;
 
 	public Reference agent;
+	public float deltaPerSecond;
 
 	private void Start() => this.agentTransform = this.agent.GameObject.transform;
 
+	private Vector3 Interpolate(in Vector3 target) => Vector3.MoveTowards(
+		this.agentTransform.position,
+		target,
+		this.deltaPerSecond * Time.fixedDeltaTime
+	);
+
 	public void BeginMoveTo(Vector3 position)
 	{
-		this.agentTransform.position = position;
+		this.agentTransform.position = this.deltaPerSecond == default
+			? position
+			: this.Interpolate(position);
 	}
 }
--- Assets/Scripts/Tests/MonoBehaviour/MoveTowardsOngoingMBTests.cs
+++ Assets/Scripts/Tests/MonoBehaviour/MoveTowardsOngoingMBTests.cs
@@ -22,4 +22,25 @@ public class MoveTowardsOngoingMBTests : TestCollection
 
 		Tools.AssertEqual(position, agent.transform.position);
 	}
+
+	[UnityTest]
+	public IEnumerator MoveOneFrameSpeed()
+	{
+		var position = new Vector3(10, 0, 0);
+		var agent = new GameObject("agent");
+		var mover = new GameObject("mover").AddComponent<MoveTowardsOngoingMB>();
+		mover.agent = agent;
+		mover.deltaPerSecond = 10;
+
+		yield return new WaitForEndOfFrame();
+
+		mover.BeginMoveTo(position);
+
+		yield return new WaitForFixedUpdate();
+
+		Tools.AssertEqual(
+			Vector3.right * Time.fixedDeltaTime * 10,
+			agent.transform.position
+		);
+	}
 }
