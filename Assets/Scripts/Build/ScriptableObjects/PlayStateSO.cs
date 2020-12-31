89e4935f634efcba66a1dc5b77dddc952fb5e4c4
codaishin <codaishin@gmail.com>
2020-12-31 12:58:16 +0100

    PlayStateSO: State property with OnStateChange event

--- Assets/Scripts/Build/ScriptableObjects/PlayStateSO.cs
+++ Assets/Scripts/Build/ScriptableObjects/PlayStateSO.cs
@@ -1,8 +1,24 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
+public enum PlayState { None = default, Play, Paused, Menu }
+
 public class PlayStateSO : ScriptableObject
 {
+	private PlayState state;
 
+	public PlayState State {
+		get => this.state;
+		set => this.ApplyState(value);
+	}
+
+	public event Action<PlayState> OnStateChange;
+
+	private void ApplyState(in PlayState state)
+	{
+		this.state = state;
+		this.OnStateChange?.Invoke(state);
+	}
 }
--- Assets/Scripts/Tests/ScriptableObjects/PlayStateSOTests.cs
+++ Assets/Scripts/Tests/ScriptableObjects/PlayStateSOTests.cs
@@ -6,5 +6,33 @@ using UnityEngine.TestTools;
 
 public class PlayStateSOTests : TestCollection
 {
+	[Test]
+	public void OnPlayStateChange()
+	{
+		var called = PlayState.None;
+		var stateSO = ScriptableObject.CreateInstance<PlayStateSO>();
 
+		stateSO.OnStateChange += v => called = v;
+		stateSO.State = PlayState.Paused;
+
+		Assert.AreEqual(PlayState.Paused, called);
+	}
+
+	[Test]
+	public void EmptyOnStateChange()
+	{
+		var stateSO = ScriptableObject.CreateInstance<PlayStateSO>();
+
+		Assert.DoesNotThrow(() => stateSO.State = PlayState.Paused);
+	}
+
+	[Test]
+	public void StatePropertyReflectsState()
+	{
+		var stateSO = ScriptableObject.CreateInstance<PlayStateSO>();
+
+		stateSO.State = PlayState.Paused;
+
+		Assert.AreEqual(PlayState.Paused, stateSO.State);
+	}
 }
