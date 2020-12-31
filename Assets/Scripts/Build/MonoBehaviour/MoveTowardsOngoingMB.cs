d9056935ffbeddc451e1e7892ff8517440585764
codaishin <codaishin@gmail.com>
2020-12-31 11:25:38 +0100

    MoveTowardsOngoingMB: Infinite instant move

--- Assets/Scripts/Build/MonoBehaviour/MoveTowardsOngoingMB.cs
+++ Assets/Scripts/Build/MonoBehaviour/MoveTowardsOngoingMB.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class MoveTowardsOngoingMB : MonoBehaviour
 {
+	private Transform agentTransform;
 
+	public Reference agent;
+
+	private void Start() => this.agentTransform = this.agent.GameObject.transform;
+
+	public void BeginMoveTo(Vector3 position)
+	{
+		this.agentTransform.position = position;
+	}
 }
--- Assets/Scripts/Tests/MonoBehaviour/MoveTowardsOngoingMBTests.cs
+++ Assets/Scripts/Tests/MonoBehaviour/MoveTowardsOngoingMBTests.cs
@@ -6,5 +6,20 @@ using UnityEngine.TestTools;
 
 public class MoveTowardsOngoingMBTests : TestCollection
 {
+	[UnityTest]
+	public IEnumerator MoveOneFrame()
+	{
+		var position = new Vector3(1, 0, 0);
+		var agent = new GameObject("agent");
+		var mover = new GameObject("mover").AddComponent<MoveTowardsOngoingMB>();
+		mover.agent = agent;
 
+		yield return new WaitForEndOfFrame();
+
+		mover.BeginMoveTo(position);
+
+		yield return new WaitForFixedUpdate();
+
+		Tools.AssertEqual(position, agent.transform.position);
+	}
 }
