bb39095554b567436888c8229db374bde4c7aa64
codaishin <codaishin@gmail.com>
2021-01-01 14:41:19 +0100

    TrackerMB: Implemented simple Tracking

--- Assets/Scripts/Build/MonoBehaviour/TrackerMB.cs
+++ Assets/Scripts/Build/MonoBehaviour/TrackerMB.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class TrackerMB : MonoBehaviour
 {
+	private Transform agentTransform;
+	private Transform targetTransform;
 
+	public Reference agent;
+	public Reference target;
+
+	private void Start()
+	{
+		this.agentTransform = this.agent.GameObject.transform;
+		this.targetTransform = this.target.GameObject.transform;
+	}
+
+	public void Track()
+	{
+		this.agentTransform.position = this.targetTransform.position;
+	}
 }
--- Assets/Scripts/Tests/MonoBehaviour/TrackerMBTests.cs
+++ Assets/Scripts/Tests/MonoBehaviour/TrackerMBTests.cs
@@ -6,5 +6,21 @@ using UnityEngine.TestTools;
 
 public class TrackerMBTests : TestCollection
 {
+	[UnityTest]
+	public IEnumerator TrackPosition()
+	{
+		var trackerMB = new GameObject("tracker").AddComponent<TrackerMB>();
+		var agent = new GameObject("agent");
+		var target = new GameObject("target");
 
+		trackerMB.agent = agent;
+		trackerMB.target = target;
+
+		yield return new WaitForEndOfFrame();
+
+		target.transform.position = Vector3.right;
+		trackerMB.Track();
+
+		Tools.AssertEqual(Vector3.right, agent.transform.position);
+	}
 }
