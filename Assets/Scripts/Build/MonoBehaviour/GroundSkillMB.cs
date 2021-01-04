20754f6b712a61484240f705aa4d472c098f9d23
codaishin <codaishin@gmail.com>
2021-01-04 13:40:06 +0100

    GroundSkillMB: Implemented

--- Assets/Scripts/Build/MonoBehaviour/GroundSkillMB.cs
+++ Assets/Scripts/Build/MonoBehaviour/GroundSkillMB.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class GroundSkillMB : MonoBehaviour
 {
+	private GameObject selectorObject;
 
+	public SkillMB skill;
+	public Reference selector;
+
+	private void Start() => this.selectorObject = this.selector.GameObject;
+
+	public void Apply(Vector3 position)
+	{
+		this.selectorObject.transform.position = position;
+		this.skill.Apply(this.selectorObject);
+	}
 }
--- Assets/Scripts/Tests/MonoBehaviour/GroundSkillMBTests.cs
+++ Assets/Scripts/Tests/MonoBehaviour/GroundSkillMBTests.cs
@@ -6,5 +6,56 @@ using UnityEngine.TestTools;
 
 public class GroundSkillMBTests : TestCollection
 {
+	private class MockSkillBehaviourSO : BaseSkillBehaviourSO
+	{
+		public GameObject agent;
+		public GameObject target;
 
+		public override void Apply(in GameObject agent, in GameObject target)
+		{
+			this.agent = agent;
+			this.target = target;
+		}
+	}
+
+	[UnityTest]
+	public IEnumerator CallSelector()
+	{
+		var groundSkill = new GameObject("groundSkill").AddComponent<GroundSkillMB>();
+		var skill = new GameObject("skill").AddComponent<SkillMB>();
+		var behaviour = ScriptableObject.CreateInstance<MockSkillBehaviourSO>();
+
+		skill.agent = new GameObject("agent");
+		skill.behaviour = behaviour;
+		groundSkill.skill = skill;
+		groundSkill.selector = new GameObject("selector");
+
+		yield return new WaitForEndOfFrame();
+
+		groundSkill.Apply(default);
+
+		Assert.AreSame(groundSkill.selector.GameObject, behaviour.target);
+	}
+
+	[UnityTest]
+	public IEnumerator UpdaSelectorPosition()
+	{
+		var groundSkill = new GameObject("groundSkill").AddComponent<GroundSkillMB>();
+		var skill = new GameObject("skill").AddComponent<SkillMB>();
+		var behaviour = ScriptableObject.CreateInstance<MockSkillBehaviourSO>();
+
+		skill.agent = new GameObject("agent");
+		skill.behaviour = behaviour;
+		groundSkill.skill = skill;
+		groundSkill.selector = new GameObject("selector");
+
+		yield return new WaitForEndOfFrame();
+
+		groundSkill.Apply(Vector3.up);
+
+		Assert.AreEqual(
+			Vector3.up,
+			groundSkill.selector.GameObject.transform.position
+		);
+	}
 }
