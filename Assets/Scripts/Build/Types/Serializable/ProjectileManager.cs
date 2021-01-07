592079869d6b43545ce02d5f5614781d72172ad7
codaishin <codaishin@gmail.com>
2021-01-07 19:07:55 +0100

    ProjectileManager: Implemented spawn and Move behaviour

--- Assets/Scripts/Build/Types/Serializable/ProjectileManager.cs
+++ Assets/Scripts/Build/Types/Serializable/ProjectileManager.cs
@@ -1,12 +1,64 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
 public class ProjectileManager : IProjectileManager
 {
+	private List<Transform> projectiles = new List<Transform>();
+
+	public Transform spawnPoint;
+	public GameObject prefab;
+	public float deltaPerSecond;
+
+	public IEnumerable<Transform> Projectiles => this.projectiles;
+
+	private Vector3 Interpolate(in Transform current, in Transform target) =>
+ 		Vector3.MoveTowards(
+			current.position,
+			target.position,
+			this.deltaPerSecond * Time.fixedDeltaTime
+		);
+
+	private bool FirstInactiveProjectile(out Transform projectile)
+	{
+		projectile = this.projectiles
+			.Where(p => !p.gameObject.activeSelf)
+			.FirstOrDefault();
+		return projectile;
+	}
+
+	private Transform GetOrMakeProjectile()
+	{
+		if (!this.FirstInactiveProjectile(out Transform projectile)) {
+			projectile = GameObject.Instantiate(this.prefab).transform;
+			this.projectiles.Add(projectile);
+		}
+		projectile.position = this.spawnPoint.position;
+		return projectile;
+	}
+
+	private void Activate(in Transform projectile)
+	{
+		projectile.gameObject.SetActive(true);
+		projectile.parent = null;
+	}
+
+	private void Deactivate(in Transform projectile)
+	{
+		projectile.gameObject.SetActive(false);
+		projectile.parent = this.spawnPoint;
+	}
+
 	public IEnumerator<WaitForFixedUpdate> ProjectileRoutineTo(Transform target)
 	{
-		yield break;
+		Transform projectile = this.GetOrMakeProjectile();
+		this.Activate(projectile);
+		while (projectile.position != target.position) {
+			projectile.position = this.Interpolate(projectile, target);
+			yield return new WaitForFixedUpdate();
+		}
+		this.Deactivate(projectile);
 	}
 }
--- Assets/Scripts/Tests/Types/Serializable/ProjectileManagerTests.cs
+++ Assets/Scripts/Tests/Types/Serializable/ProjectileManagerTests.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
@@ -6,5 +6,248 @@ using UnityEngine.TestTools;
 
 public class ProjectileManagerTests : TestCollection
 {
+	private class MockPrefabMB : MonoBehaviour {}
 
+	[Test]
+	public void ProjectileInstantiate()
+	{
+		var spawn = new GameObject("self");
+		var target = new GameObject("target");
+		var prefab = new GameObject("prefab");
+		var manager = new ProjectileManager();
+
+		manager.spawnPoint = spawn.transform;
+		manager.prefab = prefab;
+		manager.ProjectileRoutineTo(target.transform).MoveNext();
+
+		Assert.AreEqual(1, manager.Projectiles.Count());
+	}
+
+	[Test]
+	public void ProjectileInstantiatePrefab()
+	{
+		var spawn = new GameObject("self");
+		var target = new GameObject("target");
+		var prefab = new GameObject("prefab").AddComponent<MockPrefabMB>();
+		var manager = new ProjectileManager();
+
+		manager.spawnPoint = spawn.transform;
+		manager.prefab = prefab.gameObject;
+		manager.ProjectileRoutineTo(target.transform).MoveNext();
+
+		Assert.True(manager.Projectiles.First().TryGetComponent(out MockPrefabMB _));
+	}
+
+	[Test]
+	public void MoveProjectileToTarget()
+	{
+		var spawn = new GameObject("self");
+		var target = new GameObject("target");
+		var prefab = new GameObject("prefab");
+		var manager = new ProjectileManager();
+
+		target.transform.position = Vector3.right;
+		manager.spawnPoint = spawn.transform;
+		manager.prefab = prefab;
+		manager.deltaPerSecond = 1;
+		var iterator = manager.ProjectileRoutineTo(target.transform);
+
+		iterator.MoveNext();
+
+		Tools.AssertEqual(
+			Vector3.right * Time.fixedDeltaTime,
+			manager.Projectiles.First().transform.position
+		);
+	}
+
+	[Test]
+	public void MoveProjectileFullyToTarget()
+	{
+		var spawn = new GameObject("self");
+		var target = new GameObject("target");
+		var prefab = new GameObject("prefab");
+		var manager = new ProjectileManager();
+
+		target.transform.position = Vector3.right;
+		manager.spawnPoint = spawn.transform;
+		manager.prefab = prefab;
+		manager.deltaPerSecond = 0.5f;
+		var iterator = manager.ProjectileRoutineTo(target.transform);
+
+		while (iterator.MoveNext());
+
+		Tools.AssertEqual(
+			target.transform.position,
+			manager.Projectiles.First().transform.position
+		);
+	}
+
+	[Test]
+	public void DisableProjectileWhenTargetReached()
+	{
+		var spawn = new GameObject("self");
+		var target = new GameObject("target");
+		var prefab = new GameObject("prefab");
+		var manager = new ProjectileManager();
+
+		target.transform.position = Vector3.right;
+		manager.spawnPoint = spawn.transform;
+		manager.prefab = prefab;
+		manager.deltaPerSecond = 0.5f;
+		var iterator = manager.ProjectileRoutineTo(target.transform);
+
+		while (iterator.MoveNext());
+
+		Assert.False(manager.Projectiles.First().gameObject.activeSelf);
+	}
+
+	[Test]
+	public void AttachProjectileWhenTargetReached()
+	{
+		var spawn = new GameObject("self");
+		var target = new GameObject("target");
+		var prefab = new GameObject("prefab");
+		var manager = new ProjectileManager();
+
+		target.transform.position = Vector3.right;
+		manager.spawnPoint = spawn.transform;
+		manager.prefab = prefab;
+		manager.deltaPerSecond = 0.5f;
+		var iterator = manager.ProjectileRoutineTo(target.transform);
+
+		while (iterator.MoveNext());
+
+		Assert.AreSame(spawn.transform, manager.Projectiles.First().parent);
+	}
+
+	[Test]
+	public void ProjectileNotDisabledInFlight()
+	{
+		var spawn = new GameObject("self");
+		var target = new GameObject("target");
+		var prefab = new GameObject("prefab");
+		var manager = new ProjectileManager();
+
+		target.transform.position = Vector3.right;
+		manager.spawnPoint = spawn.transform;
+		manager.prefab = prefab;
+		manager.deltaPerSecond = 0.5f;
+		var iterator = manager.ProjectileRoutineTo(target.transform);
+		var states = new List<bool>();
+
+		while (iterator.MoveNext()) {
+			states.Add(manager.Projectiles.First().gameObject.activeSelf);
+		}
+
+		Assert.True(states.All(s => s));
+	}
+
+	[Test]
+	public void ProjectileDetachedInFlight()
+	{
+		var spawn = new GameObject("self");
+		var target = new GameObject("target");
+		var prefab = new GameObject("prefab");
+		var manager = new ProjectileManager();
+
+		target.transform.position = Vector3.right;
+		manager.spawnPoint = spawn.transform;
+		manager.prefab = prefab;
+		manager.deltaPerSecond = 0.5f;
+		var iterator = manager.ProjectileRoutineTo(target.transform);
+		var states = new List<Transform>();
+
+		while (iterator.MoveNext()) {
+			states.Add(manager.Projectiles.First().parent);
+		}
+
+		Assert.True(states.All(s => s == null));
+	}
+
+	[Test]
+	public void ReuseProjectile()
+	{
+		var spawn = new GameObject("self");
+		var target = new GameObject("target");
+		var prefab = new GameObject("prefab");
+		var manager = new ProjectileManager();
+
+		target.transform.position = Vector3.right;
+		manager.spawnPoint = spawn.transform;
+		manager.prefab = prefab;
+		manager.deltaPerSecond = 0.5f;
+		var iterator = manager.ProjectileRoutineTo(target.transform);
+		var states = new List<bool>();
+
+		while (iterator.MoveNext());
+
+		manager.ProjectileRoutineTo(target.transform).MoveNext();
+
+		Assert.AreEqual(1, manager.Projectiles.Count());
+	}
+
+	[Test]
+	public void ReusedProjectileActive()
+	{
+		var spawn = new GameObject("self");
+		var target = new GameObject("target");
+		var prefab = new GameObject("prefab");
+		var manager = new ProjectileManager();
+
+		target.transform.position = Vector3.right;
+		manager.spawnPoint = spawn.transform;
+		manager.prefab = prefab;
+		manager.deltaPerSecond = 0.5f;
+		var iterator = manager.ProjectileRoutineTo(target.transform);
+		var states = new List<bool>();
+
+		while (iterator.MoveNext());
+
+		target.transform.position += Vector3.one;
+		manager.ProjectileRoutineTo(target.transform).MoveNext();
+
+		Assert.True(manager.Projectiles.First().gameObject.activeSelf);
+	}
+
+	[Test]
+	public void ReusedProjectileAtSpawnPosition()
+	{
+		var spawn = new GameObject("self");
+		var target = new GameObject("target");
+		var prefab = new GameObject("prefab");
+		var manager = new ProjectileManager();
+
+		target.transform.position = Vector3.right;
+		manager.spawnPoint = spawn.transform;
+		manager.prefab = prefab;
+		manager.deltaPerSecond = 0.5f;
+		var iterator = manager.ProjectileRoutineTo(target.transform);
+		var states = new List<bool>();
+
+		while (iterator.MoveNext());
+
+		spawn.transform.position = Vector3.left;
+		target.transform.position = Vector3.left;
+		manager.ProjectileRoutineTo(target.transform).MoveNext();
+
+		Assert.AreEqual(Vector3.left, manager.Projectiles.First().position);
+	}
+
+	[Test]
+	public void UseNewProjectile()
+	{
+		var spawn = new GameObject("self");
+		var target = new GameObject("target");
+		var prefab = new GameObject("prefab");
+		var manager = new ProjectileManager();
+
+		target.transform.position = Vector3.right;
+		manager.spawnPoint = spawn.transform;
+		manager.prefab = prefab;
+		manager.deltaPerSecond = 0.5f;
+		manager.ProjectileRoutineTo(target.transform).MoveNext();
+		manager.ProjectileRoutineTo(target.transform).MoveNext();
+
+		Assert.AreEqual(2, manager.Projectiles.Count());
+	}
 }
