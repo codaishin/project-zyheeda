58874523abf397b73ab5478e9a9a328af28725db
codaishin <codaishin@gmail.com>
2021-01-08 18:29:50 +0100

    ProjectilePathing: Made a class again to avoid selfcopy when using "this" in projectile routine

--- Assets/Scripts/Build/Types/Serializable/ProjectilePathing.cs
+++ Assets/Scripts/Build/Types/Serializable/ProjectilePathing.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public struct ProjectilePathing : IProjectilePathing
+public class ProjectilePathing : IProjectilePathing
 {
 	public Transform spawnPoint;
 	public BaseMagazineMB magazine;
