d315143612b38f8dd2fd206c98fd282ffb29205b
codaishin <codaishin@gmail.com>
2021-01-08 17:39:09 +0100

    BaseHitableMB: DisallowMultipleComponent

--- Assets/Scripts/Tests/MonoBehaviour/BaseHitableMBTests.cs
+++ Assets/Scripts/Tests/MonoBehaviour/BaseHitableMBTests.cs
@@ -6,5 +6,15 @@ using UnityEngine.TestTools;
 
 public class BaseHitableMBTests
 {
+	private class MockHitableMB : BaseHitableMB {}
 
+	[Test]
+	public void OnlyOneHitable()
+	{
+		var hitable = new GameObject("hitable").AddComponent<MockHitableMB>();
+
+		hitable.gameObject.AddComponent<MockHitableMB>();
+
+		Assert.AreEqual(1, hitable.gameObject.GetComponents<MockHitableMB>().Count());
+	}
 }
