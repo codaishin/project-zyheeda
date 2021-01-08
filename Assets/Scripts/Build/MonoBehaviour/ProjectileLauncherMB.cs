3e7a16b3ddd3d07ff6c44294e9aa7a143c46bcc9
codaishin <codaishin@gmail.com>
2021-01-08 17:13:37 +0100

    ProjectileLauncherMB: Added

---
+++ Assets/Scripts/Build/MonoBehaviour/ProjectileLauncherMB.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLauncherMB : BaseItemMB
+{
+	public override
+	IEnumerator<WaitForFixedUpdate> Apply(SkillMB skill, GameObject target)
+	{
+		yield break;
+	}
+}
