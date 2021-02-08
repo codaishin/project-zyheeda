de219f5181910fa580f127b36bdfb4462fc9e443
codaishin <codaishin@gmail.com>
2021-02-08 11:32:26 +0100

    OnMouseOverMB: Implemented

--- Assets/Scripts/Build/MonoBehaviour/OnMouseOverMB.cs
+++ Assets/Scripts/Build/MonoBehaviour/OnMouseOverMB.cs
@@ -1,8 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class OnMouseOverMB : MonoBehaviour
 {
+	public UnityEvent onMouseEnter;
+	public UnityEvent onMouseExit;
 
+	private void Start()
+	{
+		if (this.onMouseEnter == null) {
+			this.onMouseEnter = new UnityEvent();
+		}
+		if (this.onMouseExit == null) {
+			this.onMouseExit = new UnityEvent();
+		}
+	}
+
+	private void OnMouseEnter() => this.onMouseEnter.Invoke();
+
+	private void OnMouseExit() => this.onMouseExit.Invoke();
 }
--- Assets/Scripts/Tests/MonoBehaviour/OnMouseOverMBTests.cs
+++ Assets/Scripts/Tests/MonoBehaviour/OnMouseOverMBTests.cs
@@ -1,10 +1,67 @@
 using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
 public class OnMouseOverMBTests
 {
+	[UnityTest]
+	public IEnumerator NotNull()
+	{
+		var onMouseOver = new GameObject("obj").AddComponent<OnMouseOverMB>();
 
+		yield return new WaitForEndOfFrame();
+
+		CollectionAssert.AreEqual(
+			new object[] { true, true },
+			new object[] {
+				onMouseOver.onMouseEnter != null,
+				onMouseOver.onMouseExit != null
+			}
+		);
+	}
+
+	[Test]
+	public void CallbacksNull()
+	{
+		var onMouseOver = new GameObject("obj").AddComponent<OnMouseOverMB>();
+		CollectionAssert.AreEqual(
+			new object[] {null, null},
+			new object[] { onMouseOver.onMouseEnter, onMouseOver.onMouseExit }
+		);
+	}
+
+	[UnityTest]
+	public IEnumerator OnMouseEnter()
+	{
+		var called = 0;
+		var onMouseOver = new GameObject("obj").AddComponent<OnMouseOverMB>();
+
+		yield return new WaitForEndOfFrame();
+
+		onMouseOver.onMouseEnter.AddListener(() => ++called);
+
+		onMouseOver.Invoke("OnMouseEnter", 0);
+
+		yield return new WaitForEndOfFrame();
+
+		Assert.AreEqual(1, called);
+	}
+
+	[UnityTest]
+	public IEnumerator OnMouseExit()
+	{
+		var called = 0;
+		var onMouseOver = new GameObject("obj").AddComponent<OnMouseOverMB>();
+
+		yield return new WaitForEndOfFrame();
+
+		onMouseOver.onMouseExit.AddListener(() => ++called);
+
+		onMouseOver.Invoke("OnMouseExit", 0);
+
+		yield return new WaitForEndOfFrame();
+
+		Assert.AreEqual(1, called);
+	}
 }
