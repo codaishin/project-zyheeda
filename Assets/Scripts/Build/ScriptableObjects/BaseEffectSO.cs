32b32c8f2dc01c86be6a0aec51e83335634eb25b
codaishin <codaishin@gmail.com>
2021-02-10 14:28:26 +0100

    BaseEffectSO: Removed item property

--- Assets/Scripts/Build/ScriptableObjects/BaseEffectSO.cs
+++ Assets/Scripts/Build/ScriptableObjects/BaseEffectSO.cs
@@ -2,7 +2,5 @@ using UnityEngine;
 
 public abstract class BaseEffectSO : ScriptableObject
 {
-	public BaseItemBehaviourMB Item { get; private set; }
-
 	public abstract void Apply(in SkillMB skill, in GameObject target);
 }
