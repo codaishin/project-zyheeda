a32224b4033ac654feea4bfb727579ffb64e2b1f
codaishin <codaishin@gmail.com>
2021-02-15 11:52:10 +0100

    GameObjectExtensions: Implemente RequireComponent

--- Assets/Scripts/Build/Tools/GameObjectExtensions.cs
+++ Assets/Scripts/Build/Tools/GameObjectExtensions.cs
@@ -1,8 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public static class GameObjectExtensions
 {
-
+	public static T RequireComponent<T>(this GameObject gameObject)
+	{
+		if (gameObject.TryGetComponent(out T component)) {
+			return component;
+		}
+		throw new MissingComponentException(
+			$"GameObject \"{gameObject.name}\" does not have a Component of type \"{typeof(T)}\""
+		);
+	}
 }
--- Assets/Scripts/Tests/Tools/GameObjectExtensionsTests.cs
+++ Assets/Scripts/Tests/Tools/GameObjectExtensionsTests.cs
@@ -1,10 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.TestTools;
 
 public class GameObjectExtensionsTests : TestCollection
 {
+	private class MockMB : MonoBehaviour {}
 
+	[Test]
+	public void RequireComponent()
+	{
+		var obj = new GameObject("obj");
+		var cmp = obj.AddComponent<MockMB>();
+
+		Assert.AreSame(cmp, obj.RequireComponent<MockMB>());
+	}
+
+	[Test]
+	public void RequireComponentMissing()
+	{
+		var obj = new GameObject("obj");
+		Assert.Throws<MissingComponentException>(() => obj.RequireComponent<MockMB>());
+	}
+
+	[Test]
+	public void RequireComponentMissingMessage()
+	{
+		var obj = new GameObject("obj");
+		try {
+			obj.RequireComponent<MockMB>();
+		} catch (MissingComponentException e) {
+			Assert.AreEqual(
+				"GameObject \"obj\" does not have a Component of type \"GameObjectExtensionsTests+MockMB\"",
+				e.Message
+			);
+		}
+	}
 }
