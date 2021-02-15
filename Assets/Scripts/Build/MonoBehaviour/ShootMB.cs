8a053de6ec1ee61f1f42248bddb800881e90d5e8
codaishin <codaishin@gmail.com>
2021-02-15 16:04:46 +0100

    ShootMB: obsolote -> removed

--- Assets/Scripts/Build/MonoBehaviour/ShootMB.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using System.Collections.Generic;
-using UnityEngine;
-
-[Serializable]
-public class ProjectileLauncher : BaseProjectileLauncher<ProjectilePathing> {}
-
-public class ShootMB : BaseSkillMB<ProjectileLauncher> { }
