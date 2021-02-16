248169bc46c6176a387fd18688fa0e262673f8d9
codaishin <codaishin@gmail.com>
2021-02-16 13:27:09 +0100

    Implemeted stats adding

--- Assets/Scripts/Build/Types/Serializable/Stats.cs
+++ Assets/Scripts/Build/Types/Serializable/Stats.cs
@@ -1,8 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
 
+[Serializable]
 public struct Stats
 {
+	public int body;
+	public int mind;
+	public int spirit;
 
+	public static Stats operator+(in Stats a, in Stats b) => new Stats {
+		body = a.body + b.body,
+		mind = a.mind + b.mind,
+		spirit = a.spirit + b.spirit,
+	};
 }
--- Assets/Scripts/Tests/Types/Serializable/StatsTests.cs
+++ Assets/Scripts/Tests/Types/Serializable/StatsTests.cs
@@ -6,5 +6,16 @@ using UnityEngine.TestTools;
 
 public class StatsTests : TestCollection
 {
+	[Test]
+	public void Add()
+	{
+		var a = new Stats { body = 10, mind = 20, spirit = 44 };
+		var b = new Stats { body = 11, mind = -5, spirit = 42 };
+		var c = a + b;
 
+		Assert.AreEqual(
+			(21, 15, 86),
+			(c.body, c.mind, c.spirit)
+		);
+	}
 }
