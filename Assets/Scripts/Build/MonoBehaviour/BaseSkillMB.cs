8c8e20d38dec0bf1feb6980c1443acf068e5aa16
codaishin <codaishin@gmail.com>
2021-02-16 16:17:55 +0100

    BaseSkillMB: Implemented

--- Assets/Scripts/Build/MonoBehaviour/BaseSkillMB.cs
+++ Assets/Scripts/Build/MonoBehaviour/BaseSkillMB.cs
@@ -2,7 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class BaseSkillMB : MonoBehaviour
+public abstract class BaseSkillMB<TEffect, TCast> : MonoBehaviour
+	where TEffect : IEffect, new()
+	where TCast : ICast, new()
 {
+	private IItem item;
+	private float cooldown;
 
+	public float applyPerSecond;
+	public Attributes modifiers;
+	public TEffect effect = new TEffect();
+	public TCast cast = new TCast();
+
+	private IEnumerable<WaitForFixedUpdate> Cast(GameObject target)
+	{
+		IEnumerator<WaitForFixedUpdate> routine = this.cast.Apply(target);
+		while (routine.MoveNext()) {
+			yield return routine.Current;
+			this.cooldown -= Time.fixedDeltaTime;
+		}
+	}
+
+	private void ApplyEffect(in GameObject target, in IHitable hitable)
+	{
+		Attributes combined = this.item.Attributes + this.modifiers;
+		if (hitable.TryHit(combined)) {
+			this.effect.Apply(target, combined);
+		}
+	}
+
+	private IEnumerable<WaitForFixedUpdate> AfterCast()
+	{
+		while (this.cooldown > 0) {
+			yield return new WaitForFixedUpdate();
+			this.cooldown -= Time.fixedDeltaTime;
+		}
+	}
+
+	private IEnumerator<WaitForFixedUpdate> Apply(GameObject target, IHitable hitable)
+	{
+		foreach (WaitForFixedUpdate yield in this.Cast(target)) {
+			yield return yield;
+		}
+		this.ApplyEffect(target, hitable);
+		foreach (WaitForFixedUpdate yield in this.AfterCast()) {
+			yield return yield;
+		}
+	}
+
+	public void Begin(GameObject target)
+	{
+		if (this.cast.Valid(target, out IHitable hitable) && this.cooldown <= 0) {
+			this.cooldown = this.applyPerSecond > 0 ? 1f / this.applyPerSecond : 0;
+			this.StartCoroutine(this.Apply(target, hitable));
+		}
+	}
+
+	private void Start()
+	{
+		this.item = this.transform.parent.RequireComponent<IItem>();
+	}
 }
--- Assets/Scripts/Tests/MonoBehaviour/BaseSkillMBTests.cs
+++ Assets/Scripts/Tests/MonoBehaviour/BaseSkillMBTests.cs
@@ -6,5 +6,333 @@ using UnityEngine.TestTools;
 
 public class BaseSkillMBTests : TestCollection
 {
+	private class MockCast : ICast
+	{
+		public delegate IEnumerator<WaitForFixedUpdate> ApplyFunc(GameObject t);
 
+		public ApplyFunc apply = MockCast.BaseApply;
+
+		private static IEnumerator<WaitForFixedUpdate> BaseApply(GameObject _)
+		{
+			yield break;
+		}
+
+		public IEnumerator<WaitForFixedUpdate> Apply(GameObject target) =>
+			this.apply(target);
+		public bool Valid(in GameObject target, out IHitable hitable) =>
+			target.TryGetComponent(out hitable);
+	}
+
+	private class MockEffect : IEffect
+	{
+		public delegate void ApplyAction(in GameObject t, in Attributes a);
+
+		public ApplyAction apply = MockEffect.BaseApply;
+
+		private static void BaseApply(in GameObject _, in Attributes __) {}
+
+		public void Apply(in GameObject target, in Attributes attributes) =>
+			this.apply(target, attributes);
+	}
+
+	private class MockItemMB : MonoBehaviour, IItem
+	{
+		public Attributes attributes;
+
+		public Attributes Attributes => this.attributes;
+	}
+
+	private class MockHitableMB : MonoBehaviour, IHitable
+	{
+		public delegate bool TryHitFunc(in int mind);
+
+		public int spirit;
+
+		public bool TryHit(in Attributes attributes) => attributes.mind >= this.spirit;
+	}
+
+	private class MockSkillMB : BaseSkillMB<MockEffect, MockCast> {}
+
+	[UnityTest]
+	public IEnumerator Begin()
+	{
+		var applied = false;
+		var target = new GameObject("target").AddComponent<MockHitableMB>();
+		var skill = new GameObject("skill").AddComponent<MockSkillMB>();
+		var item = new GameObject("item").AddComponent<MockItemMB>();
+
+		IEnumerator<WaitForFixedUpdate> applyCast(GameObject _) {
+			applied = true;
+			yield break;
+		}
+
+		skill.transform.SetParent(item.transform);
+		skill.cast.apply = applyCast;
+
+		yield return new WaitForEndOfFrame();
+
+		skill.Begin(target.gameObject);
+
+		Assert.True(applied);
+	}
+
+	[UnityTest]
+	public IEnumerator DontBeginWhenNotValid()
+	{
+		var applied = false;
+		var target = new GameObject("target");
+		var skill = new GameObject("skill").AddComponent<MockSkillMB>();
+		var item = new GameObject("item").AddComponent<MockItemMB>();
+
+		IEnumerator<WaitForFixedUpdate> applyCast(GameObject _) {
+			applied = true;
+			yield break;
+		}
+
+		skill.transform.SetParent(item.transform);
+		skill.cast.apply = applyCast;
+
+		yield return new WaitForEndOfFrame();
+
+		skill.Begin(target.gameObject);
+
+		Assert.False(applied);
+	}
+
+	[UnityTest]
+	public IEnumerator ApplyEffect()
+	{
+		var got = (default(GameObject), 0, 0, 0);
+		var target = new GameObject("target").AddComponent<MockHitableMB>();
+		var skill = new GameObject("skill").AddComponent<MockSkillMB>();
+		var item = new GameObject("item").AddComponent<MockItemMB>();
+
+		void applyEffect(in GameObject t, in Attributes a) {
+			got = (t, a.body, a.mind, a.spirit);
+		}
+
+		skill.transform.SetParent(item.transform);
+		skill.effect.apply = applyEffect;
+		item.attributes = new Attributes { body = 1, mind = 2, spirit = 3 };
+
+		yield return new WaitForEndOfFrame();
+
+		skill.Begin(target.gameObject);
+
+		Assert.AreEqual(
+			(target.gameObject, item.attributes.body, item.attributes.mind, item.attributes.spirit),
+			got
+		);
+	}
+
+	[UnityTest]
+	public IEnumerator DontApplyEffectWhenNotValid()
+	{
+		var got = (default(GameObject), 0, 0, 0);
+		var target = new GameObject("target");
+		var skill = new GameObject("skill").AddComponent<MockSkillMB>();
+		var item = new GameObject("item").AddComponent<MockItemMB>();
+
+		void applyEffect(in GameObject t, in Attributes a) {
+			got = (t, a.body, a.mind, a.spirit);
+		}
+
+		skill.transform.SetParent(item.transform);
+		skill.effect.apply = applyEffect;
+		item.attributes = new Attributes { body = 1, mind = 2, spirit = 3 };
+
+		yield return new WaitForEndOfFrame();
+
+		skill.Begin(target.gameObject);
+
+		Assert.AreEqual((default(GameObject), 0, 0, 0), got);
+	}
+
+	[UnityTest]
+	public IEnumerator DontApplyEffectWhenNotHit()
+	{
+		var got = (default(GameObject), 0, 0, 0);
+		var target = new GameObject("target").AddComponent<MockHitableMB>();
+		var skill = new GameObject("skill").AddComponent<MockSkillMB>();
+		var item = new GameObject("item").AddComponent<MockItemMB>();
+
+		void applyEffect(in GameObject t, in Attributes a) {
+			got = (t, a.body, a.mind, a.spirit);
+		}
+
+		skill.transform.SetParent(item.transform);
+		skill.effect.apply = applyEffect;
+		item.attributes = new Attributes { body = 1, mind = 2, spirit = 3 };
+		target.spirit = 3;
+
+		yield return new WaitForEndOfFrame();
+
+		skill.Begin(target.gameObject);
+
+		Assert.AreEqual((default(GameObject), 0, 0, 0), got);
+	}
+
+	[UnityTest]
+	public IEnumerator ApplyEffectAfterCast()
+	{
+		var got = new List<(GameObject, int, int, int)>();
+		var target = new GameObject("target").AddComponent<MockHitableMB>();
+		var skill = new GameObject("skill").AddComponent<MockSkillMB>();
+		var item = new GameObject("item").AddComponent<MockItemMB>();
+
+		IEnumerator<WaitForFixedUpdate> applyCast(GameObject t) {
+			got.Add((t, 0, 0, 0));
+			yield return new WaitForFixedUpdate();
+			got.Add((t, 0, 0, 0));
+			yield return new WaitForFixedUpdate();
+		}
+		void applyEffect(in GameObject t, in Attributes a) {
+			got.Add((t, a.body, a.mind, a.spirit));
+		}
+
+		skill.transform.SetParent(item.transform);
+		skill.cast.apply = applyCast;
+		skill.effect.apply = applyEffect;
+		item.attributes = new Attributes { body = 1, mind = 2, spirit = 3 };
+
+		yield return new WaitForEndOfFrame();
+
+		skill.Begin(target.gameObject);
+
+		yield return new WaitForFixedUpdate();
+		yield return new WaitForFixedUpdate();
+
+		CollectionAssert.AreEqual(
+			new (GameObject, int, int, int)[] {
+				(target.gameObject, 0, 0, 0),
+				(target.gameObject, 0, 0, 0),
+				(target.gameObject, 1, 2, 3),
+			},
+			got
+		);
+	}
+
+	[UnityTest]
+	public IEnumerator DontBeginDuringCooldown()
+	{
+		var applied = 0;
+		var target = new GameObject("target").AddComponent<MockHitableMB>();
+		var skill = new GameObject("skill").AddComponent<MockSkillMB>();
+		var item = new GameObject("item").AddComponent<MockItemMB>();
+
+		IEnumerator<WaitForFixedUpdate> applyCast(GameObject _) {
+			++applied;
+			yield break;
+		}
+
+		skill.transform.SetParent(item.transform);
+		skill.cast.apply = applyCast;
+		skill.applyPerSecond = 1;
+
+		yield return new WaitForEndOfFrame();
+
+		skill.Begin(target.gameObject);
+		skill.Begin(target.gameObject);
+
+		Assert.AreEqual(1, applied);
+	}
+
+	[UnityTest]
+	public IEnumerator BeginAfterCooldown()
+	{
+		var applied = 0;
+		var target = new GameObject("target").AddComponent<MockHitableMB>();
+		var skill = new GameObject("skill").AddComponent<MockSkillMB>();
+		var item = new GameObject("item").AddComponent<MockItemMB>();
+
+		IEnumerator<WaitForFixedUpdate> applyCast(GameObject _) {
+			++applied;
+			yield break;
+		}
+
+		skill.transform.SetParent(item.transform);
+		skill.cast.apply = applyCast;
+		skill.applyPerSecond = 10;
+
+		yield return new WaitForEndOfFrame();
+
+		skill.Begin(target.gameObject);
+
+		yield return new WaitForSeconds(0.11f);
+
+		skill.Begin(target.gameObject);
+
+		Assert.AreEqual(2, applied);
+	}
+
+	[UnityTest]
+	public IEnumerator BeginWithNoCooldown()
+	{
+		var applied = 0;
+		var target = new GameObject("target").AddComponent<MockHitableMB>();
+		var skill = new GameObject("skill").AddComponent<MockSkillMB>();
+		var item = new GameObject("item").AddComponent<MockItemMB>();
+
+		IEnumerator<WaitForFixedUpdate> applyCast(GameObject _) {
+			++applied;
+			yield break;
+		}
+
+		skill.transform.SetParent(item.transform);
+		skill.cast.apply = applyCast;
+
+		yield return new WaitForEndOfFrame();
+
+		skill.Begin(target.gameObject);
+		skill.Begin(target.gameObject);
+
+		Assert.AreEqual(2, applied);
+	}
+
+	[UnityTest]
+	public IEnumerator ModifiedAttributesInTryHit()
+	{
+		var applied = false;
+		var target = new GameObject("target").AddComponent<MockHitableMB>();
+		var skill = new GameObject("skill").AddComponent<MockSkillMB>();
+		var item = new GameObject("item").AddComponent<MockItemMB>();
+
+		void applyEffect(in GameObject _, in Attributes __) { applied = true; }
+
+		skill.transform.SetParent(item.transform);
+		skill.effect.apply = applyEffect;
+		skill.modifiers.mind = 10;
+		item.attributes.mind = 10;
+
+		target.spirit = 20;
+
+		yield return new WaitForEndOfFrame();
+
+		skill.Begin(target.gameObject);
+
+		Assert.True(applied);
+	}
+
+	[UnityTest]
+	public IEnumerator ModifiedAttributesInEffect()
+	{
+		var got = default(Attributes);
+		var target = new GameObject("target").AddComponent<MockHitableMB>();
+		var skill = new GameObject("skill").AddComponent<MockSkillMB>();
+		var item = new GameObject("item").AddComponent<MockItemMB>();
+
+		void applyEffect(in GameObject _, in Attributes a) { got = a; }
+
+		skill.transform.SetParent(item.transform);
+		skill.effect.apply = applyEffect;
+		skill.modifiers = new Attributes { body = 1, mind = 2, spirit = 3 };
+
+		item.attributes = new Attributes { body = 41, mind = 40, spirit = 39 };
+
+		yield return new WaitForEndOfFrame();
+
+		skill.Begin(target.gameObject);
+
+		Assert.AreEqual(new Attributes{ body = 42, mind = 42, spirit = 42 }, got);
+	}
 }
