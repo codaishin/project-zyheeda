a6b9eb6b9b87b65151cac86df7ba763218d31883
codaishin <codaishin@gmail.com>
2021-02-17 13:32:38 +0100

    BaseHitableMB: Removed

--- Assets/Scripts/Build/MonoBehaviour/BaseHitableMB.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[DisallowMultipleComponent]
-public abstract class BaseHitableMB : MonoBehaviour
-{
-	public abstract bool TryHit(in int offense);
-}
