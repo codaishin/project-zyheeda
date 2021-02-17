69b8185d08f783f87ca67e74c352235b2d4489e5
codaishin <codaishin@gmail.com>
2021-02-17 13:32:18 +0100

    BaseItemBehaviourSO: Removed

--- Assets/Scripts/Build/ScriptableObjects/BaseItemBehaviourSO.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class BaseItemBehaviourSO : ScriptableObject
-{
-	public abstract
-	bool Apply(SkillMB skill, GameObject target, out IEnumerator<WaitForFixedUpdate> routine);
-}
