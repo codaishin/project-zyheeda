8960ad59b3e531ae49225f947832a6941b6cf0e0
codaishin <codaishin@gmail.com>
2021-02-18 12:36:13 +0100

    DisposableExtensions: Formatting

--- Assets/Scripts/Build/Tools/DisposableExtensions.cs
+++ Assets/Scripts/Build/Tools/DisposableExtensions.cs
@@ -1,6 +1,6 @@
 public static class DisposableExtensions
 {
-  public static
+	public static
 	Disposable<T> AsDisposable<T>(this T value, in OnDisposeFunc<T> onDispose) =>
 		new Disposable<T>(value, onDispose);
 
