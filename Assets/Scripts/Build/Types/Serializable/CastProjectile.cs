8e89f3f007db7bf540fdccfbb286d93e2c909985
codaishin <codaishin@gmail.com>
2021-02-19 13:39:48 +0100

    CastProjectile: Became implementation of BaseCastProjectile

--- Assets/Scripts/Build/Types/Serializable/CastProjectile.cs
+++ Assets/Scripts/Build/Types/Serializable/CastProjectile.cs
@@ -1,27 +1,4 @@
 using System;
-using System.Collections.Generic;
-using UnityEngine;
 
 [Serializable]
-public class CastProjectile : ICast
-{
-	public GameObject projectileSpawn;
-	public MagazineMB magazine;
-	public float projectileSpeed;
-
-	public IEnumerator<WaitForFixedUpdate> Apply(GameObject target)
-	{
-		using (this.magazine.GetOrMakeProjectile().Use(out GameObject projectile)) {
-			projectile.transform.position = this.projectileSpawn.transform.position;
-			while (projectile.transform.position != target.transform.position) {
-				projectile.transform.position = Vector3.MoveTowards(
-					projectile.transform.position,
-					target.transform.position,
-					this.projectileSpeed * Time.fixedDeltaTime
-				);
-				projectile.transform.LookAt(target.transform);
-				yield return new WaitForFixedUpdate();
-			}
-		}
-	}
-}
+public class CastProjectile : BaseCastProjectile<Magazine> { }
