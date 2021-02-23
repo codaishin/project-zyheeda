6de33874d20e0042979ffd9fd2895bd70c565e3e
codaishin <codaishin@gmail.com>
2021-02-23 14:40:03 +0100

    BaseApproachMB: Removed redundant Approach property cache

--- Assets/Scripts/Build/MonoBehaviour/BaseApproachMB.cs
+++ Assets/Scripts/Build/MonoBehaviour/BaseApproachMB.cs
@@ -3,22 +3,17 @@ using UnityEngine;
 public abstract class BaseApproachMB : MonoBehaviour
 {
 	private Transform agentTransform;
-	private Movement.ApproachFunc<Vector3> approach;
 
 	public Reference agent;
 	public float deltaPerSecond;
 
 	protected abstract Movement.ApproachFunc<Vector3> Approach { get; }
 
-	private void Start()
-	{
-		this.approach = this.Approach;
-		this.agentTransform = this.agent.GameObject.transform;
-	}
+	private void Start() => this.agentTransform = this.agent.GameObject.transform;
 
 	public void Apply(Vector3 position)
 	{
 		this.StopAllCoroutines();
-		this.StartCoroutine(this.approach(this.agentTransform, position, this.deltaPerSecond));
+		this.StartCoroutine(this.Approach(this.agentTransform, position, this.deltaPerSecond));
 	}
 }
