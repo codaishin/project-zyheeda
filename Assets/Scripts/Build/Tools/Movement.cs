5048631349f349d9a76f2280a8acd3fc04b44805
codaishin <codaishin@gmail.com>
2021-02-23 12:18:52 +0100

    Movement: Implemented Towards

--- Assets/Scripts/Build/Tools/Movement.cs
+++ Assets/Scripts/Build/Tools/Movement.cs
@@ -1,8 +1,44 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public static class Movement
 {
+	public delegate IEnumerator<WaitForFixedUpdate> TowardsFunc<TTarget>(
+		Transform transform,
+		TTarget target,
+		float deltaPerSecond
+	);
 
+	public delegate TowardsFunc<TTarget> GetTowardsFunc<TTarget>(
+		Func<TTarget, Vector3> getPosition,
+		Func<float> getTimeDelta
+	);
+
+	private static
+	IEnumerator<WaitForFixedUpdate> Towards<TTarget>(
+		Func<TTarget, Vector3> getPosition,
+		Func<float> getTimeDelta,
+		Transform transform,
+		TTarget targetObject,
+		float deltaPerSecond
+	) {
+		bool notOnTarget(out Vector3 target) {
+			target = getPosition(targetObject);
+			return transform.position != target;
+		}
+
+		float delta = (deltaPerSecond > 0f ? deltaPerSecond : float.PositiveInfinity) * getTimeDelta();
+
+		while (notOnTarget(out Vector3 target)) {
+			transform.position = Vector3.MoveTowards(transform.position, target, delta);
+			yield return new WaitForFixedUpdate();
+		}
+	}
+
+	public static TowardsFunc<TTarget> Towards<TTarget>(
+		Func<TTarget, Vector3> getPosition,
+		Func<float> getTimeDelta
+	) => (transform, target, deltaPerSecond) => Movement
+		.Towards(getPosition, getTimeDelta, transform, target, deltaPerSecond);
 }
--- Assets/Scripts/Tests/Tools/MovementTowardsTests.cs
+++ Assets/Scripts/Tests/Tools/MovementTowardsTests.cs
@@ -6,5 +6,81 @@ using UnityEngine.TestTools;
 
 public class MovementTowardsTests : TestCollection
 {
+	[Test]
+	public void MovementTowards()
+	{
+		var obj = new GameObject("obj");
+		var moveTo = Movement.Towards<Vector3>(v => v, () => 1);
 
+		moveTo(obj.transform, Vector3.right, 0f).MoveNext();
+
+		Assert.AreEqual(Vector3.right, obj.transform.position);
+	}
+
+	[Test]
+	public void MovementTowardsDeltaPerSecond()
+	{
+		var obj = new GameObject("obj");
+		var moveTo = Movement.Towards<Vector3>(v => v, () => 1);
+
+		moveTo(obj.transform, Vector3.right, 0.1f).MoveNext();
+
+		Assert.AreEqual(Vector3.right * 0.1f, obj.transform.position);
+	}
+
+	[Test]
+	public void MovementTowardsDeltaPerSecondFromOffset()
+	{
+		var obj = new GameObject("obj");
+		var moveTo = Movement.Towards<Vector3>(v => v, () => 1);
+
+		obj.transform.position = Vector3.left;
+
+		moveTo(obj.transform, Vector3.right, 0.1f).MoveNext();
+
+		Assert.AreEqual(Vector3.left + Vector3.right * 0.1f, obj.transform.position);
+	}
+
+	[Test]
+	public void MovementTowardsTimeDelta()
+	{
+		var obj = new GameObject("obj");
+		var moveTo = Movement.Towards<Vector3>(v => v, () => 0.3f);
+
+		moveTo(obj.transform, Vector3.right, 1).MoveNext();
+
+		Assert.AreEqual(Vector3.right * 0.3f, obj.transform.position);
+	}
+
+	[Test]
+	public void MovementTowardsMultipleFrames()
+	{
+		var positions = new List<Vector3>();
+		var obj = new GameObject("obj");
+		var moveTo = Movement.Towards<Vector3>(v => v, () => 1);
+		var routine = moveTo(obj.transform, Vector3.right, 0.1f);
+
+		routine.MoveNext();
+
+		positions.Add(obj.transform.position);
+
+		routine.MoveNext();
+
+		positions.Add(obj.transform.position);
+
+		CollectionAssert.AreEqual(
+			new Vector3[] { new Vector3(0.1f, 0, 0), new Vector3(0.2f, 0, 0) },
+			positions
+		);
+	}
+
+	[Test]
+	public void MovementTowardsTerminates()
+	{
+		var obj = new GameObject("obj");
+		var moveTo = Movement.Towards<Vector3>(v => v, () => 1);
+		var r = moveTo(obj.transform, Vector3.right, 0.51f);
+
+		Assert.AreEqual((true, true, false), (r.MoveNext(), r.MoveNext(), r.MoveNext()));
+	}
 }
