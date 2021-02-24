d533779b7c78651994ff1a86f31486be8d2a91a8
codaishin <codaishin@gmail.com>
2021-02-24 12:34:27 +0100

    BaseApproach: Implemented

--- Assets/Scripts/Build/Types/Serializable/BaseApproach.cs
+++ Assets/Scripts/Build/Types/Serializable/BaseApproach.cs
@@ -1,10 +1,26 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class BaseApproach<TTarget> : IApproach<TTarget>
 {
+	public Action<Transform, TTarget>[] postUpdate = new Action<Transform, TTarget>[0];
+
+	protected abstract Vector3 GetPosition(in TTarget target);
+	protected abstract float GetTimeDelta();
+
 	public IEnumerator<WaitForFixedUpdate> Approach(Transform transform, TTarget target, float speed)
 	{
-		yield break;
+		bool notOnTarget(out Vector3 targetPosition) {
+			targetPosition = this.GetPosition(target);
+			return transform.position != targetPosition;
+		}
+		float delta = (speed > 0f ? speed : float.PositiveInfinity) * this.GetTimeDelta();
+
+		while (notOnTarget(out Vector3 targetPosition)) {
+			transform.position = Vector3.MoveTowards(transform.position, targetPosition, delta);
+			this.postUpdate.ForEach(cb => cb(transform, target));
+			yield return new WaitForFixedUpdate();
+		}
 	}
 }
--- Assets/Scripts/Tests/Types/Serializable/BaseApproachTests.cs
+++ Assets/Scripts/Tests/Types/Serializable/BaseApproachTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
@@ -6,5 +7,128 @@ using UnityEngine.TestTools;
 
 public class BaseApproachTests
 {
+	private class MockApproach : BaseApproach<Vector3>
+	{
+		public float timeDelta = 1f;
 
+		protected override Vector3 GetPosition(in Vector3 target) => target;
+		protected override float GetTimeDelta() => this.timeDelta;
+	}
+
+	[Test]
+	public void GetApproach()
+	{
+		var obj = new GameObject("obj");
+		var approach = new MockApproach();
+
+		approach.Approach(obj.transform, Vector3.right, 0f).MoveNext();
+
+		Assert.AreEqual(Vector3.right, obj.transform.position);
+	}
+
+
+	[Test]
+	public void GetApproachDeltaPerSecond()
+	{
+		var obj = new GameObject("obj");
+		var approach = new MockApproach();
+
+		approach.Approach(obj.transform, Vector3.right, 0.1f).MoveNext();
+
+		Assert.AreEqual(Vector3.right * 0.1f, obj.transform.position);
+	}
+
+	[Test]
+	public void GetApproachDeltaPerSecondFromOffset()
+	{
+		var obj = new GameObject("obj");
+		var approach = new MockApproach();
+
+		obj.transform.position = Vector3.left;
+
+		approach.Approach(obj.transform, Vector3.right, 0.1f).MoveNext();
+
+		Assert.AreEqual(Vector3.left + Vector3.right * 0.1f, obj.transform.position);
+	}
+
+	[Test]
+	public void GetApproachTimeDelta()
+	{
+		var obj = new GameObject("obj");
+		var approach = new MockApproach{ timeDelta = 0.3f };
+
+		approach.Approach(obj.transform, Vector3.right, 1).MoveNext();
+
+		Assert.AreEqual(Vector3.right * 0.3f, obj.transform.position);
+	}
+
+	[Test]
+	public void GetApproachMultipleFrames()
+	{
+		var positions = new List<Vector3>();
+		var obj = new GameObject("obj");
+		var approach = new MockApproach();
+		var routine = approach.Approach(obj.transform, Vector3.right, 0.1f);
+
+		routine.MoveNext();
+
+		positions.Add(obj.transform.position);
+
+		routine.MoveNext();
+
+		positions.Add(obj.transform.position);
+
+		CollectionAssert.AreEqual(
+			new Vector3[] { new Vector3(0.1f, 0, 0), new Vector3(0.2f, 0, 0) },
+			positions
+		);
+	}
+
+	[Test]
+	public void GetApproachTerminates()
+	{
+		var obj = new GameObject("obj");
+		var approach = new MockApproach();
+		var r = approach.Approach(obj.transform, Vector3.right, 0.51f);
+
+		Assert.AreEqual((true, true, false), (r.MoveNext(), r.MoveNext(), r.MoveNext()));
+	}
+
+	[Test]
+	public void GetApproachPostUpdateCalled()
+	{
+		var calledWith = new List<(Transform, Vector3)>();
+		var obj = new GameObject("obj");
+		var approach = new MockApproach();
+		var r = approach.Approach(obj.transform, Vector3.right, 0.2f);
+
+		approach.postUpdate = new Action<Transform, Vector3> [] {
+			(tr, ta) => calledWith.Add((tr, ta)),
+		};
+
+		r.MoveNext();
+		r.MoveNext();
+
+		CollectionAssert.AreEqual(
+			new (Transform, Vector3)[] { (obj.transform, Vector3.right), (obj.transform, Vector3.right) },
+			calledWith
+		);
+	}
+
+	[Test]
+	public void GetApproachPostUpdateAfterPositionUpdate()
+	{
+		var called = default(Vector3);
+		var obj = new GameObject("obj");
+		var approach = new MockApproach();
+		var r = approach.Approach(obj.transform, Vector3.right, 0.2f);
+
+		approach.postUpdate = new Action<Transform, Vector3>[] {
+			(tr, __) => called = tr.position,
+		};
+
+		r.MoveNext();
+
+		Assert.AreEqual(new Vector3(0.2f, 0, 0), called);
+	}
 }
