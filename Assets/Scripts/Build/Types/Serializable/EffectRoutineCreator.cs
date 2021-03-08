f7b849ae2d5e7a8ee2e13beece35fb1aad24d5c7
codaishin <codaishin@gmail.com>
2021-03-08 11:21:24 +0100

    EffectRoutineCreator: Added Serializable attribute

--- Assets/Scripts/Build/Types/Serializable/EffectRoutineCreator.cs
+++ Assets/Scripts/Build/Types/Serializable/EffectRoutineCreator.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public class EffectRoutineCreator : IEffectRoutineCreator
 {
 	public float intervalDelta;
