6a4f870c0b74cce190c110ed4afec19a799b2332
codaishin <codaishin@gmail.com>
2021-03-10 10:15:57 +0100

    BaseEffectBehaviourSO: Added tag and stacking

--- Assets/Scripts/Build/ScriptableObjects/BaseEffectBehaviourSO.cs
+++ Assets/Scripts/Build/ScriptableObjects/BaseEffectBehaviourSO.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public abstract class BaseEffectBehaviourSO : ScriptableObject
 {
+	public EffectTag tag;
+	public ConditionStacking stacking;
+
 	public abstract void Apply(CharacterSheetMB source, CharacterSheetMB target, int intensity);
 	public abstract void Maintain(CharacterSheetMB source, CharacterSheetMB target, int intensity, float intervalDelta);
 	public abstract void Revert(CharacterSheetMB source, CharacterSheetMB target, int intensity);
