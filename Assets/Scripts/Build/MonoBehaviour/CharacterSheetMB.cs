06143aec4f86fbf644244f300c76ca4005da31ad
codaishin <codaishin@gmail.com>
2021-03-11 15:04:54 +0100

    CharacterSheetMB: Implementes ISections

--- Assets/Scripts/Build/MonoBehaviour/CharacterSheetMB.cs
+++ Assets/Scripts/Build/MonoBehaviour/CharacterSheetMB.cs
@@ -2,19 +2,33 @@ using System;
 using UnityEngine;
 
 [RequireComponent(typeof(IntensityManagerMB), typeof(DurationManagerMB))]
-public class CharacterSheetMB : MonoBehaviour, IConditionManager
+public class CharacterSheetMB : MonoBehaviour, IConditionManager, ISections
 {
+	private const string stackingExcFmt = "Invalid stacking {0} for {1}";
+	private const string requireExcFmt = "{0} is not a valid section for {1}";
+
 	private IntensityManagerMB stackIntensity;
 	private DurationManagerMB stackDuration;
+
+	private ArgumentException Error<T>(string fmt, T value) => new ArgumentException(string.Format(fmt, value, this));
 
 	public void Add(Effect condition)
 	{
 		IConditionManager stack = condition.stacking switch {
 			ConditionStacking.Duration => this.stackDuration,
 			ConditionStacking.Intensity => this.stackIntensity,
-			_ => throw new ArgumentException($"Invalid stacking {condition.stacking} for {this.name} (CharacterSheetMB)"),
+			_ => throw this.Error(CharacterSheetMB.stackingExcFmt, condition.stacking),
 		};
 		stack.Add(condition);
+	}
+
+	public void UseSection<TSection>(SectionAction<TSection> action, bool required)
+	{
+		Action run = action switch {
+			_ when required => () => throw this.Error(CharacterSheetMB.requireExcFmt, typeof(TSection)),
+			_ => () => { },
+		};
+		run();
 	}
 
 	private void Awake()
--- Assets/Scripts/Tests/MonoBehaviour/CharacterSheetMBTests.cs
+++ Assets/Scripts/Tests/MonoBehaviour/CharacterSheetMBTests.cs
@@ -69,4 +69,34 @@ public class CharacterSheetMBTests : TestCollection
 			Assert.AreEqual("Invalid stacking -1 for obj (CharacterSheetMB)", e.Message);
 		}
 	}
+
+	private class MockClass {}
+
+	[Test]
+	public void UseSectionRequiredFail()
+	{
+		var sheet = new GameObject("obj").AddComponent<CharacterSheetMB>();
+
+		Assert.Throws<ArgumentException>(() => sheet.UseSection<MockClass>((ref MockClass _) => {}, true));
+	}
+
+	[Test]
+	public void UseSectionRequiredFailMessage()
+	{
+		var sheet = new GameObject("obj").AddComponent<CharacterSheetMB>();
+
+		try {
+			sheet.UseSection<MockClass>((ref MockClass _) => {}, true);
+		} catch (ArgumentException e) {
+			Assert.AreEqual($"{typeof(MockClass)} is not a valid section for obj (CharacterSheetMB)", e.Message);
+		}
+	}
+
+	[Test]
+	public void UseSectionRequiredFailWithoutTrhow()
+	{
+		var sheet = new GameObject("obj").AddComponent<CharacterSheetMB>();
+
+		Assert.DoesNotThrow(() => sheet.UseSection<MockClass>((ref MockClass _) => {}, false));
+	}
 }
