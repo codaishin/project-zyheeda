eee4685a6c2562df452a2269111e4089e1db3beb
codaishin <codaishin@gmail.com>
2021-03-13 14:04:01 +0100

    Resistance: Basic Implementation

---
+++ Assets/Scripts/Build/Types/Serializable/Resistance.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq;
+using UnityEngine;
+
+[Serializable]
+public struct Resistance
+{
+	[Serializable]
+	public struct Data
+	{
+		public EffectTag tag;
+		[Range(0f, 1f)]
+		public float value;
+	}
+
+	public Data[] data;
+
+	public float this[EffectTag tag] => this.data != null
+		? this.data.Where(d => d.tag == tag).FirstOrDefault().value
+		: 0f;
+}
