5046d6a395315d9146d3d520410106a1e388ade5
codaishin <codaishin@gmail.com>
2021-03-15 13:47:08 +0100

    BaseModHpSO: Removed CreateAssetMenu Attribute

--- Assets/Scripts/Build/ScriptableObjects/BaseModHpSO.cs
+++ Assets/Scripts/Build/ScriptableObjects/BaseModHpSO.cs
@@ -1,8 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "ScriptableObjects/EffectBehaviour/ModHp")]
 public class BaseModHpSO<TResistance> : BaseEffectBehaviourSO
 	where TResistance : IResistance
 {
