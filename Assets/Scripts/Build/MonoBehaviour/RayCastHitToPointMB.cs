378a93c4c54760fa7599894bcb05f03eacdf41bf
codaishin <codaishin@gmail.com>
2021-03-23 15:06:25 +0100

    RayCastHotToPointMB: Implements BaseMorphMB

--- Assets/Scripts/Build/MonoBehaviour/RayCastHitToPointMB.cs
+++ Assets/Scripts/Build/MonoBehaviour/RayCastHitToPointMB.cs
@@ -1,9 +1,6 @@
 using UnityEngine;
 
-public class RayCastHitToPointMB : BaseConditionalMorphMB<RaycastHit, Vector3>
+public class RayCastHitToPointMB : BaseMorphMB<RaycastHit, Vector3>
 {
-	public override bool TryMorph(RaycastHit hit, out Vector3 got) {
-		got = hit.point;
-		return true;
-	}
+	public override Vector3 DoMorph(RaycastHit hit) => hit.point;
 }
