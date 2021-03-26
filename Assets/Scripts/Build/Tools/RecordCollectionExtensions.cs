63a37823ac33b66d923caf669af18207464ac4e4
codaishin <codaishin@gmail.com>
2021-03-26 13:03:20 +0100

    RecordCollectioNExtensions: Validate trace replaced with simple list

--- Assets/Scripts/Build/Tools/RecordCollectionExtensions.cs
+++ Assets/Scripts/Build/Tools/RecordCollectionExtensions.cs
@@ -6,11 +6,11 @@ public static class RecordCollectionExtensions
 	public static
 	IEnumerable<Record<TKey, TValue>> Validate<TKey, TValue>(this IEnumerable<Record<TKey, TValue>> records)
 	{
-		HashSet<TKey> track = new HashSet<TKey>();
+		List<TKey> trace = new List<TKey>();
 
 		Record<TKey, TValue> markDuplicates(Record<TKey, TValue> item) {
-			item.name = track.Contains(item.key) ? "__duplicate__" : item.key.ToString();
-			track.Add(item.key);
+			item.name = trace.Contains(item.key) ? "__duplicate__" : item.key.ToString();
+			trace.Add(item.key);
 			return item;
 		}
 
