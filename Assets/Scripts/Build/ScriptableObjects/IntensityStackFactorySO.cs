5e981d8ec1501dc7d0ed7c3f7a0da2d01af29835
codaishin <codaishin@gmail.com>
2021-03-31 14:21:45 +0200

    IntensityStack: refactored as IntensityStackFactory

---
+++ Assets/Scripts/Build/ScriptableObjects/IntensityStackFactorySO.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntensityStackFactorySO : ScriptableObject, IStackFactory
+{
+	private class Stack : IStack
+	{
+		private Dictionary<Effect, Finalizable> effects = new Dictionary<Effect, Finalizable>();
+		private Func<Effect, Finalizable> effectToRoutine;
+		private Action<Finalizable> onPull;
+		private Action<Finalizable> onCancel;
+
+		public IEnumerable<Effect> Effects => this.effects.Keys;
+
+		public Stack(Func<Effect, Finalizable> effectToRoutine, Action<Finalizable> onPull, Action<Finalizable> onCancel)
+		{
+			this.effectToRoutine = effectToRoutine;
+			this.onPull = onPull;
+			this.onCancel = onCancel;
+		}
+
+		public void Cancel()
+		{
+			this.effects.ForEach(kvp => {
+				kvp.Key.Revert();
+				this.onCancel?.Invoke(kvp.Value);
+			});
+			this.effects.Clear();
+		}
+
+		public void Push(Effect effect)
+		{
+			Finalizable routine = this.effectToRoutine(effect);
+			routine.OnFinalize += () => effects.Remove(effect);
+			effects[effect] = routine;
+			onPull?.Invoke(routine);
+		}
+	}
+
+	public IStack Create(
+		Func<Effect, Finalizable> effectToRoutine,
+		Action<Finalizable> onPull = default,
+		Action<Finalizable> onCancel = default
+	) => new Stack(effectToRoutine, onPull, onCancel);
+}
---
+++ Assets/Scripts/Tests/ScriptableObjects/IntensityStackFactorySOTests.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+
+public class IntensityStackFactorySOTests : TestCollection
+{
+	[Test]
+	public void RoutineFromEffect()
+	{
+		IEnumerator create(Effect effect) {
+			effect.Apply();
+			yield break;
+		}
+		var called = false;
+		var factory = ScriptableObject.CreateInstance<IntensityStackFactorySO>();
+		var stack = factory.Create(
+			effectToRoutine: e => new Finalizable{ wrapped = create(e) },
+			onPull: r => r.MoveNext()
+		);
+
+		stack.Push(new Effect(apply: () => called = true));
+
+		Assert.True(called);
+	}
+
+	[Test]
+	public void CancelCallWithPushRoutine()
+	{
+		IEnumerator create() { yield break; };
+		var routines = (onPush: default(Finalizable), onCancel: default(Finalizable));
+		var factory = ScriptableObject.CreateInstance<IntensityStackFactorySO>();
+		var stack = factory.Create(
+			effectToRoutine: _ => new Finalizable{ wrapped = create() },
+			onPull: r => routines.onPush = r,
+			onCancel: r => routines.onCancel = r
+		);
+
+		stack.Push(new Effect());
+		stack.Cancel();
+
+		Assert.AreSame(routines.onPush, routines.onCancel);
+	}
+
+	[Test]
+	public void EmptyOnPullDoesNotThrow()
+	{
+		IEnumerator create() { yield break; };
+		var factory = ScriptableObject.CreateInstance<IntensityStackFactorySO>();
+		var stack = factory.Create(effectToRoutine: _ => new Finalizable{ wrapped = create() });
+
+		Assert.DoesNotThrow(() => stack.Push(new Effect()));
+	}
+
+	[Test]
+	public void EmptyCancelDoesNotThrow()
+	{
+		IEnumerator create() { yield break; };
+		var factory = ScriptableObject.CreateInstance<IntensityStackFactorySO>();
+		var stack = factory.Create(effectToRoutine: _ => new Finalizable{ wrapped = create() });
+
+		stack.Push(new Effect());
+		Assert.DoesNotThrow(() => stack.Cancel());
+	}
+
+	[Test]
+	public void NoOnCancelWhenNoCancel()
+	{
+		IEnumerator create() { yield break; };
+		var called = false;
+		var factory = ScriptableObject.CreateInstance<IntensityStackFactorySO>();
+		var stack = factory.Create(
+			effectToRoutine: _ => new Finalizable{ wrapped = create() },
+			onCancel: _ => called = true
+		);
+
+		stack.Push(new Effect());
+
+		Assert.False(called);
+	}
+
+	[Test]
+	public void GetEffects()
+	{
+		IEnumerator create() { yield break; };
+		var factory = ScriptableObject.CreateInstance<IntensityStackFactorySO>();
+		var stack = factory.Create(effectToRoutine: _ => new Finalizable{ wrapped = create() });
+		var effects = new Effect[] {
+			new Effect(),
+			new Effect(),
+		};
+
+		stack.Push(effects[0]);
+		stack.Push(effects[1]);
+
+		CollectionAssert.AreEqual(effects, stack.Effects);
+	}
+
+	[Test]
+	public void CancelCallsRevert()
+	{
+		IEnumerator create() { yield break; };
+		var called = 0;
+		var factory = ScriptableObject.CreateInstance<IntensityStackFactorySO>();
+		var stack = factory.Create(effectToRoutine: _ => new Finalizable{ wrapped = create() });
+		var effects = new Effect[] {
+			new Effect(revert: () => ++called),
+			new Effect(revert: () => ++called),
+		};
+
+		stack.Push(effects[0]);
+		stack.Push(effects[1]);
+		stack.Cancel();
+
+		Assert.AreEqual(2, called);
+	}
+
+	[Test]
+	public void CancelClearsEffects()
+	{
+		IEnumerator create() { yield break; };
+		var factory = ScriptableObject.CreateInstance<IntensityStackFactorySO>();
+		var stack = factory.Create(effectToRoutine: _ => new Finalizable{ wrapped = create() });
+		var effects = new Effect[] {
+			new Effect(),
+			new Effect(),
+		};
+
+		stack.Push(effects[0]);
+		stack.Push(effects[1]);
+		stack.Cancel();
+
+		Assert.IsEmpty(stack.Effects);
+	}
+
+	[Test]
+	public void FinishedEffectRemovedFromEffects()
+	{
+		IEnumerator create() {
+			yield return null;
+			yield return null;
+			yield return null;
+		}
+		var routine = default(Finalizable);
+		var factory = ScriptableObject.CreateInstance<IntensityStackFactorySO>();
+		var stack = factory.Create(
+			effectToRoutine: _ => new Finalizable{ wrapped = create() },
+			onPull: r => routine = r
+		);
+
+		stack.Push(new Effect());
+
+		routine.MoveNext();
+		routine.MoveNext();
+		routine.MoveNext();
+		routine.MoveNext();
+
+		Assert.IsEmpty(stack.Effects);
+	}
+}
