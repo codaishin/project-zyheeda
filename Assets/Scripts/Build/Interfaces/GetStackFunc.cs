175c24e9797cd6347749cce9da9d6bf2cfb5ba92
codaishin <codaishin@gmail.com>
2021-04-02 11:38:27 +0200

    Replaced IStackFactory with GetStackFunc delegate

---
+++ Assets/Scripts/Build/Interfaces/GetStackFunc.cs
@@ -0,0 +1,7 @@
+using System;
+
+public delegate IStack GetStackFunc(
+	Func<Effect, Finalizable> effectToRoutine,
+	Action<Finalizable> onPull = default,
+	Action<Finalizable> onCancel = default
+);
