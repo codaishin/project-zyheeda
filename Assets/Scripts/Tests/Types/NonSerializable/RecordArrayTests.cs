b59475e2aed0a6cabb09042417c1fa7e56fe304e
codaishin <codaishin@gmail.com>
2021-04-09 12:03:17 +0200

    refactor(NonSerializable): RecordsArray internalized source records and renamed to RecordArray

---
+++ Assets/Scripts/Tests/Types/NonSerializable/RecordArrayTests.cs
@@ -0,0 +1,78 @@
+using System.Linq;
+using NUnit.Framework;
+
+public class RecordArrayTests : TestCollection
+{
+	[Test]
+	public void GetEmptyValue()
+	{
+		var dict = new RecordArray<int, string>();
+		Assert.AreEqual(null, dict[20]);
+	}
+
+	[Test]
+	public void Records()
+	{
+		var records = new Record<int, string>[] {
+			new Record<int, string> { key = 2, value = "two" },
+			new Record<int, string> { key = 2, value = "other two" },
+		};
+		var dict = new RecordArray<int, string>(records);
+
+		CollectionAssert.AreEqual(records, dict.Records);
+	}
+
+	[Test]
+	public void SetValue()
+	{
+		var dict = new RecordArray<int, string>();
+
+		dict[20] = "Hello";
+
+		CollectionAssert.AreEqual(
+			new (int, string)[] { (20, "Hello") },
+			dict.Records.Select(r => (r.key, r.value))
+		);
+	}
+
+	[Test]
+	public void UpdateValue()
+	{
+		var dict = new RecordArray<int, string>();
+
+		dict[20] = "Hello";
+		dict[20] += ", dict!";
+
+		CollectionAssert.AreEqual(
+			new (int, string)[] { (20, "Hello, dict!") },
+			dict.Records.Select(r => (r.key, r.value))
+		);
+	}
+
+	[Test]
+	public void SetNames()
+	{
+		var records = new Record<int, string>[] {
+			new Record<int, string> { key = 2, value = "two" },
+			new Record<int, string> { key = 3, value = "three" },
+		};
+		var dict = new RecordArray<int, string>(records);
+		dict.SetNames(default);
+
+		CollectionAssert.AreEqual(new string[] { "2", "3" }, dict.Records.Select(r => r.name));
+	}
+
+	[Test]
+	public void SetDuplicateNames()
+	{
+		var records = new Record<int, string>[] {
+			new Record<int, string> { key = 2, value = "two" },
+			new Record<int, string> { key = 2, value = "other two" },
+			new Record<int, string> { key = 3, value = "three" },
+		};
+		var dict = new RecordArray<int, string>(records);
+		dict.SetNames("_");
+
+		CollectionAssert.AreEqual(new string[] { "2", "_", "3" }, dict.Records.Select(r => r.name));
+	}
+}
