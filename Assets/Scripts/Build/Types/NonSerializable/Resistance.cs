8f41cf62f0bc3cc0e2f370069fde22d8ff499d13
codaishin <codaishin@gmail.com>
2021-04-09 12:59:26 +0200

    refactor(Types): Made RecordArray and its implementations serializable

--- Assets/Scripts/Build/Types/NonSerializable/Resistance.cs
+++
@@ -1,6 +0,0 @@
-using System;
-
-public class Resistance : RecordsArray<EffectTag, float>
-{
-	public Resistance(Func<Record<EffectTag, float>[]> get, Action<Record<EffectTag, float>[]> set) : base(get, set) {}
-}
