6721f068918b578b4df1428498576ca4186910bb
codaishin <codaishin@gmail.com>
2021-04-25 14:24:47 +0200

    feat(ScriptableObject): TargetingSO

---
+++ Assets/Scripts/Build/ScriptableObjects/TargetingSO.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetingSO : BaseTargetingSO
+{
+	public BaseHitSO hitter;
+	public EventSO selectTarget;
+	public EventSO cancelSelect;
+	public bool upToMaxCount;
+
+	public override
+	IEnumerator<WaitForEndOfFrame> Select(CharacterSheetMB source, List<CharacterSheetMB> targets)
+	{
+		bool canceled = false;
+
+		void select() {
+			if (this.hitter.Hit.Try(source, out CharacterSheetMB target)) {
+				if (this.upToMaxCount && targets.Contains(target)) {
+					maxCount = targets.Count;
+				} else {
+					targets.Add(target);
+				}
+			}
+		}
+		void cancel() {
+			if (targets.Count > 0) {
+				targets.RemoveAt(targets.Count - 1);
+			} else {
+				canceled = true;
+			}
+		}
+
+		this.selectTarget.Listeners += select;
+		this.cancelSelect.Listeners += cancel;
+
+		while(targets.Count < maxCount && !canceled) {
+			yield return new WaitForEndOfFrame();
+		}
+
+		this.selectTarget.Listeners -= select;
+		this.cancelSelect.Listeners -= cancel;
+	}
+}
