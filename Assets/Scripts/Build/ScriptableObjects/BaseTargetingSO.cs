865637127a2d306717ec5aac4cb2fff7deb0d96d
codaishin <codaishin@gmail.com>
2021-04-30 18:19:07 +0200

    feat(BaseTargetingSO): Add on begin and end select events

--- Assets/Scripts/Build/ScriptableObjects/BaseTargetingSO.cs
+++ Assets/Scripts/Build/ScriptableObjects/BaseTargetingSO.cs
@@ -1,11 +1,20 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public abstract class BaseTargetingSO<TSheet> : ScriptableObject, ITargeting<TSheet, WaitForEndOfFrame>
 {
+	public UnityEvent onBeginSelect;
+	public UnityEvent onEndSelect;
+
 	public IEnumerator<WaitForEndOfFrame> Select(TSheet source, List<TSheet> targets,int maxCount = 1)
 	{
-		return this.DoSelect(source, targets, maxCount);
+		IEnumerator<WaitForEndOfFrame> routine = this.DoSelect(source, targets, maxCount);
+		this.onBeginSelect?.Invoke();
+		while (routine.MoveNext()) {
+			yield return routine.Current;
+		}
+		this.onEndSelect?.Invoke();
 	}
 
 	protected abstract
