516d3ebf3ad6510c095a5c3691db4f8da9b8301f
codaishin <codaishin@gmail.com>
2021-12-19 13:16:16 +0100

    refactor(scripts): TDD for PlayerAnimator

--- Assets/Scripts/Build/MonoBehaviour/Animator/PlayerAnimatorMB.cs
+++ Assets/Scripts/Build/MonoBehaviour/Animator/PlayerAnimatorMB.cs
@@ -1,12 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimatorMB : MonoBehaviour
+public abstract class BasePlayerAnimatorMB<TAnimator> :
+	BaseAnimatorMB<TAnimator>
+	where TAnimator : IAnimator
 {
-	public Animator? animator;
-
 	public void Walk(bool value) {
 		this.animator!.SetBool("walk", value);
 	}
 }
+
+public class PlayerAnimatorMB : BasePlayerAnimatorMB<WAnimator> { }
