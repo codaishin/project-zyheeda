990abc6c21f67218944bf9fb4ee4936e4faa238c
codaishin <codaishin@gmail.com>
2022-02-27 19:15:56 +0100

    feat(scripts): add MoveConstantSpeedSO

---
+++ Assets/Scripts/Tests/ScriptableObjects/Instructions/MoveConstantSpeedSOTests.cs
@@ -0,0 +1,206 @@
+using System;
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class MoveConstantSpeedSOTests : TestCollection
+{
+	class MockHitSO : BaseHitSO
+	{
+		public Func<Transform, Vector3?> getPoint = _ => null;
+
+		public override Maybe<Vector3> TryPoint(Transform source) {
+			Vector3? point = this.getPoint(source);
+			return point.HasValue ? Maybe.Some(point.Value) : Maybe.None<Vector3>();
+		}
+
+		public override Maybe<T> Try<T>(T source) {
+			throw new NotImplementedException();
+		}
+	}
+
+	class MockMB : MonoBehaviour { }
+
+	[UnityTest]
+	public IEnumerator PassTransformToHitter() {
+		Transform? transform = null;
+		var moveSO = ScriptableObject.CreateInstance<MoveConstantSpeedSO>();
+		var hitSO = ScriptableObject.CreateInstance<MockHitSO>();
+		var agent = new GameObject();
+		var runner = new GameObject().AddComponent<MockMB>();
+		var getRoutine = moveSO.InstructionsFor(agent);
+		moveSO.hitter = hitSO;
+		hitSO.getPoint = t => { transform = t; return null; };
+
+		yield return new WaitForEndOfFrame();
+
+		runner.StartCoroutine(getRoutine().GetEnumerator());
+
+		yield return new WaitForEndOfFrame();
+
+		Assert.AreSame(agent.transform, transform);
+	}
+
+	[UnityTest]
+	public IEnumerator MoveRight() {
+		var moveSO = ScriptableObject.CreateInstance<MoveConstantSpeedSO>();
+		var hitSO = ScriptableObject.CreateInstance<MockHitSO>();
+		var agent = new GameObject();
+		var runner = new GameObject().AddComponent<MockMB>();
+		var getRoutine = moveSO.InstructionsFor(agent);
+		moveSO.hitter = hitSO;
+		hitSO.getPoint = _ => Vector3.right;
+
+		yield return new WaitForEndOfFrame();
+
+		runner.StartCoroutine(getRoutine().GetEnumerator());
+
+		var delta = Time.fixedDeltaTime;
+
+		yield return new WaitForFixedUpdate();
+
+		Assert.AreEqual(Vector3.right * delta, agent.transform.position);
+	}
+
+	[UnityTest]
+	public IEnumerator MoveFromOffCenterRight() {
+		var moveSO = ScriptableObject.CreateInstance<MoveConstantSpeedSO>();
+		var hitSO = ScriptableObject.CreateInstance<MockHitSO>();
+		var agent = new GameObject();
+		var runner = new GameObject().AddComponent<MockMB>();
+		var getRoutine = moveSO.InstructionsFor(agent);
+		moveSO.hitter = hitSO;
+		hitSO.getPoint = _ => new Vector3(1, 1, 0);
+		agent.transform.position = new Vector3(1, 0, 0);
+
+		yield return new WaitForEndOfFrame();
+
+		runner.StartCoroutine(getRoutine().GetEnumerator());
+
+		var delta = Time.fixedDeltaTime;
+
+		yield return new WaitForFixedUpdate();
+
+		Assert.AreEqual(new Vector3(1, delta, 0), agent.transform.position);
+	}
+
+	[UnityTest]
+	public IEnumerator MoveRightTwice() {
+		var moveSO = ScriptableObject.CreateInstance<MoveConstantSpeedSO>();
+		var hitSO = ScriptableObject.CreateInstance<MockHitSO>();
+		var agent = new GameObject();
+		var routineRunner = new GameObject().AddComponent<MockMB>();
+		var getRoutine = moveSO.InstructionsFor(agent);
+		moveSO.hitter = hitSO;
+		hitSO.getPoint = _ => Vector3.right;
+
+		yield return new WaitForEndOfFrame();
+
+		routineRunner.StartCoroutine(getRoutine().GetEnumerator());
+
+		var delta = Time.fixedDeltaTime;
+
+		yield return new WaitForFixedUpdate();
+
+		delta += Time.fixedDeltaTime;
+
+		yield return new WaitForFixedUpdate();
+
+		Assert.AreEqual(Vector3.right * delta, agent.transform.position);
+	}
+
+	[UnityTest]
+	public IEnumerator MoveRightTwiceFaster() {
+		var moveSO = ScriptableObject.CreateInstance<MoveConstantSpeedSO>();
+		var hitSO = ScriptableObject.CreateInstance<MockHitSO>();
+		var agent = new GameObject();
+		var routineRunner = new GameObject().AddComponent<MockMB>();
+		var getRoutine = moveSO.InstructionsFor(agent);
+		moveSO.hitter = hitSO;
+		moveSO.speed = 2f;
+		hitSO.getPoint = _ => Vector3.right;
+
+		yield return new WaitForEndOfFrame();
+
+		routineRunner.StartCoroutine(getRoutine().GetEnumerator());
+
+		var delta = Time.fixedDeltaTime;
+
+		yield return new WaitForFixedUpdate();
+
+		delta += Time.fixedDeltaTime;
+
+		yield return new WaitForFixedUpdate();
+
+		Assert.AreEqual(Vector3.right * delta * 2, agent.transform.position);
+	}
+
+	[UnityTest]
+	public IEnumerator NoMoveWhenNoHit() {
+		var moveSO = ScriptableObject.CreateInstance<MoveConstantSpeedSO>();
+		var hitSO = ScriptableObject.CreateInstance<MockHitSO>();
+		var agent = new GameObject();
+		var routineRunner = new GameObject().AddComponent<MockMB>();
+		var getRoutine = moveSO.InstructionsFor(agent);
+		moveSO.hitter = hitSO;
+		moveSO.speed = 2f;
+		hitSO.getPoint = _ => null;
+		agent.transform.position = Vector3.up;
+
+		yield return new WaitForEndOfFrame();
+
+		routineRunner.StartCoroutine(getRoutine().GetEnumerator());
+
+		yield return new WaitForFixedUpdate();
+
+		Assert.AreEqual(Vector3.up, agent.transform.position);
+	}
+
+	[UnityTest]
+	public IEnumerator StopWhenOnTarget() {
+		var moveSO = ScriptableObject.CreateInstance<MoveConstantSpeedSO>();
+		var hitSO = ScriptableObject.CreateInstance<MockHitSO>();
+		var agent = new GameObject();
+		var routineRunner = new GameObject().AddComponent<MockMB>();
+		var getRoutine = moveSO.InstructionsFor(agent);
+		moveSO.hitter = hitSO;
+		moveSO.speed = float.MaxValue;
+		hitSO.getPoint = _ => Vector3.right;
+
+		yield return new WaitForEndOfFrame();
+
+		routineRunner.StartCoroutine(getRoutine().GetEnumerator());
+
+		yield return new WaitForFixedUpdate();
+
+		agent.transform.position = Vector3.zero;
+
+		yield return new WaitForFixedUpdate();
+
+		Assert.AreEqual(Vector3.zero, agent.transform.position);
+	}
+
+	[UnityTest]
+	public IEnumerator FaceTarget() {
+		var moveSO = ScriptableObject.CreateInstance<MoveConstantSpeedSO>();
+		var hitSO = ScriptableObject.CreateInstance<MockHitSO>();
+		var agent = new GameObject();
+		var routineRunner = new GameObject().AddComponent<MockMB>();
+		var getRoutine = moveSO.InstructionsFor(agent);
+		moveSO.hitter = hitSO;
+		moveSO.speed = float.MaxValue;
+		hitSO.getPoint = _ => Vector3.right;
+
+		yield return new WaitForEndOfFrame();
+
+		routineRunner.StartCoroutine(getRoutine().GetEnumerator());
+
+		yield return new WaitForEndOfFrame();
+
+		Assert.True(
+			Vector3.right == agent.transform.forward,
+			$"{Vector3.right} vs {agent.transform.forward}"
+		);
+	}
+}
