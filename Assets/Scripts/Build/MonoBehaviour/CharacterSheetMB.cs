395879847201e4625845867703111fd2a3375c1d
codaishin <codaishin@gmail.com>
2022-03-02 18:38:39 +0100

    revert(scripts): simplify CharacterSheetMB

--- Assets/Scripts/Build/MonoBehaviour/CharacterSheetMB.cs
+++ Assets/Scripts/Build/MonoBehaviour/CharacterSheetMB.cs
@@ -1,27 +1,6 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterSheetMB : MonoBehaviour, ISections
+public class CharacterSheetMB : MonoBehaviour
 {
 	public Health health;
-
-	public Action UseSection<TSection>(
-		RefAction<TSection> action,
-		Action? fallback = default
-	) {
-		return action switch {
-			RefAction<Health> use
-				=> () => use(ref this.health),
-			_ => this.UseOrThrow<TSection>(fallback),
-		};
-	}
-
-	private Action UseOrThrow<TSection>(Action? fallback) {
-		return fallback ?? throw new KeyNotFoundException(
-			$"no Section of type {typeof(TSection)} on {this}"
-		);
-	}
-
-	private void Awake() { }
 }
