bfe60ea60da545b4581614326e814ea052f77036
codaishin <codaishin@gmail.com>
2022-03-05 13:18:50 +0100

    feat(assets): Add move animation instruction plugins

--- Assets/Scripts/Build/ScriptableObjects/Instructions/Plugins/BaseInstructionsPluginSO.cs
+++ Assets/Scripts/Build/ScriptableObjects/Instructions/Plugins/BaseInstructionsPluginSO.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 
 public abstract class BaseInstructionsPluginSO : ScriptableObject
 {
-	public abstract Action GetOnBegin(GameObject agent);
-	public abstract Action GetOnEnd(GameObject agent);
+	public abstract Action? GetOnBegin(GameObject agent);
+	public abstract Action? GetOnEnd(GameObject agent);
 }
