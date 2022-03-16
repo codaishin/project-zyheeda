306cc6c44e69f3d85e98d5f2923fb4f153f18b5f
codaishin <codaishin@gmail.com>
2022-03-16 16:38:01 +0100

    refactor(scripts): InputListenerMB call release

--- Assets/Scripts/Build/MonoBehaviour/Listeners/InputListenerMB.cs
+++ Assets/Scripts/Build/MonoBehaviour/Listeners/InputListenerMB.cs
@@ -1,23 +1,28 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine.InputSystem;
 
 public class InputListenerMB : BaseListenerMB
 {
 	public BaseInputConfigSO? inputConfigSO;
+	public bool callRelease = false;
 	public InputEnum.Action[] listenTo = new InputEnum.Action[0];
 	public Reference<IApplicable>[] apply = new Reference<IApplicable>[0];
 
 	private InputAction? input;
 	private Action? applyAll = null;
+	private Action? releaseAll = null;
 	private bool triggeredThisFrame = false;
 
 	protected override void StartListening() {
 		this.input!.performed += this.InvokeOnInput;
+		this.input!.canceled += this.InvokeOnRelease;
 	}
 
 	protected override void StopListening() {
 		this.input!.performed -= this.InvokeOnInput;
+		this.input!.canceled -= this.InvokeOnRelease;
 	}
 
 	private void InvokeOnInput(InputAction.CallbackContext _) {
@@ -28,11 +33,35 @@ public class InputListenerMB : BaseListenerMB
 		this.triggeredThisFrame = true;
 	}
 
+	private void InvokeOnRelease(InputAction.CallbackContext _) {
+		if (!this.callRelease || this.releaseAll is null) {
+			return;
+		}
+		this.releaseAll();
+	}
+
+	private static Action GetApply(IApplicable applicable) {
+		return applicable.Apply;
+	}
+
+	private static Action GetRelease(IApplicable applicable) {
+		return applicable.Release;
+	}
+
+	private static Action ConcatActions(Action? fst, Action snd) {
+		return fst + snd;
+	}
+
 	protected override void Start() {
 		this.input = this.inputConfigSO![this.listenTo[0]];
-		this.applyAll = this.apply
-			.Select(a => a.Value!)
-			.Aggregate(this.applyAll, (l, c) => l + c.Apply);
+
+		IEnumerable<IApplicable> applyItems = this.apply.Select(a => a.Value!);
+		this.applyAll = applyItems
+			.Select(InputListenerMB.GetApply)
+			.Aggregate(null as Action, InputListenerMB.ConcatActions);
+		this.releaseAll = applyItems
+			.Select(InputListenerMB.GetRelease)
+			.Aggregate(null as Action, InputListenerMB.ConcatActions);
 		base.Start();
 	}
 
--- Assets/Scripts/Tests/MonoBehaviour/Listeners/InputListenerMBTests.cs
+++ Assets/Scripts/Tests/MonoBehaviour/Listeners/InputListenerMBTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -39,11 +40,10 @@ public class InputListenerMBTests : TestCollection
 
 	class MockApplicableMB : MonoBehaviour, IApplicable
 	{
-		public int called = 0;
-		public void Apply() => ++this.called;
-		public void Release() {
-			throw new NotImplementedException();
-		}
+		public int calledApply = 0;
+		public int calledRelease = 0;
+		public void Apply() => ++this.calledApply;
+		public void Release() => ++this.calledRelease;
 	}
 
 	private Gamepad? pad;
@@ -79,7 +79,7 @@ public class InputListenerMBTests : TestCollection
 
 		yield return new WaitForEndOfFrame();
 
-		Assert.AreEqual(1, apply.called);
+		Assert.AreEqual(1, apply.calledApply);
 	}
 
 	[UnityTest]
@@ -103,7 +103,7 @@ public class InputListenerMBTests : TestCollection
 
 		yield return new WaitForEndOfFrame();
 
-		Assert.AreEqual(1, apply.called);
+		Assert.AreEqual(1, apply.calledApply);
 	}
 
 	[UnityTest]
@@ -138,7 +138,7 @@ public class InputListenerMBTests : TestCollection
 
 		yield return new WaitForEndOfFrame();
 
-		Assert.AreEqual(1, apply.called);
+		Assert.AreEqual(1, apply.calledApply);
 	}
 
 	[UnityTest]
@@ -179,6 +179,105 @@ public class InputListenerMBTests : TestCollection
 
 		yield return new WaitForEndOfFrame();
 
-		Assert.AreEqual(2, apply.called);
+		Assert.AreEqual(2, apply.calledApply);
+	}
+
+	[UnityTest]
+	public IEnumerator CallRelease() {
+		var so = ScriptableObject.CreateInstance<MockInputConfigSO>();
+		var mb = new GameObject("obj").AddComponent<InputListenerMB>();
+		var apply = new GameObject("apply").AddComponent<MockApplicableMB>();
+		mb.apply = new Reference<IApplicable>[] {
+			Reference<IApplicable>.PointToComponent(apply),
+		};
+		mb.inputConfigSO = so;
+		mb.listenTo = new InputEnum.Action[] { InputEnum.Action.Walk };
+		mb.callRelease = true;
+		so.actions.Values.ForEach(a => a.Enable());
+
+		yield return new WaitForEndOfFrame();
+
+		InputSystem.QueueStateEvent(
+			this.pad!,
+			new GamepadState { leftTrigger = 1f }
+		);
+
+		InputSystem.QueueStateEvent(
+			this.pad!,
+			new GamepadState { leftTrigger = 0f }
+		);
+
+		yield return new WaitForEndOfFrame();
+
+		Assert.AreEqual(1, apply.calledRelease);
+	}
+
+
+	[UnityTest]
+	public IEnumerator CallReleaseMultiple() {
+		var so = ScriptableObject.CreateInstance<MockInputConfigSO>();
+		var mb = new GameObject("obj").AddComponent<InputListenerMB>();
+		var apply = new MockApplicableMB[] {
+			 new GameObject("apply a").AddComponent<MockApplicableMB>(),
+			 new GameObject("apply b").AddComponent<MockApplicableMB>(),
+		};
+		mb.apply = apply.Select(Reference<IApplicable>.PointToComponent).ToArray();
+		mb.inputConfigSO = so;
+		mb.listenTo = new InputEnum.Action[] { InputEnum.Action.Walk };
+		mb.callRelease = true;
+		so.actions.Values.ForEach(a => a.Enable());
+
+		yield return new WaitForEndOfFrame();
+
+		InputSystem.QueueStateEvent(
+			this.pad!,
+			new GamepadState { leftTrigger = 1f }
+		);
+
+		InputSystem.QueueStateEvent(
+			this.pad!,
+			new GamepadState { leftTrigger = 0f }
+		);
+
+		yield return new WaitForEndOfFrame();
+
+		CollectionAssert.AreEqual(
+			new int[] { 1, 1 },
+			apply.Select(a => a.calledRelease)
+		);
+	}
+
+	[UnityTest]
+	public IEnumerator DoNotCallRelease() {
+		var so = ScriptableObject.CreateInstance<MockInputConfigSO>();
+		var mb = new GameObject("obj").AddComponent<InputListenerMB>();
+		var apply = new MockApplicableMB[] {
+			 new GameObject("apply a").AddComponent<MockApplicableMB>(),
+			 new GameObject("apply b").AddComponent<MockApplicableMB>(),
+		};
+		mb.apply = apply.Select(Reference<IApplicable>.PointToComponent).ToArray();
+		mb.inputConfigSO = so;
+		mb.listenTo = new InputEnum.Action[] { InputEnum.Action.Walk };
+		mb.callRelease = false;
+		so.actions.Values.ForEach(a => a.Enable());
+
+		yield return new WaitForEndOfFrame();
+
+		InputSystem.QueueStateEvent(
+			this.pad!,
+			new GamepadState { leftTrigger = 1f }
+		);
+
+		InputSystem.QueueStateEvent(
+			this.pad!,
+			new GamepadState { leftTrigger = 0f }
+		);
+
+		yield return new WaitForEndOfFrame();
+
+		CollectionAssert.AreEqual(
+			new int[] { 0, 0 },
+			apply.Select(a => a.calledRelease)
+		);
 	}
 }
