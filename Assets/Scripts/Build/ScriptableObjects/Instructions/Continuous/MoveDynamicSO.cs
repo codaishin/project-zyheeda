0a5057b29af1a0ad2b28ff3e4ba9f8a34261441a
codaishin <codaishin@gmail.com>
2022-03-17 22:35:55 +0100

    feat(scripts): add MoveDynamicSO

---
+++ Assets/Scripts/Build/ScriptableObjects/Instructions/Continuous/MoveDynamicSO.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Instructions/MoveDynamic")]
+public class MoveDynamicSO : BaseInstructionsSO<Transform>
+{
+	[Serializable]
+	public struct ValueSet
+	{
+		public float distance;
+		public float speed;
+		public float weight;
+	}
+
+	public BaseHitSO? hitter;
+	public ValueSet min;
+	public ValueSet max;
+
+	protected override Transform GetConcreteAgent(GameObject agent) {
+		return agent.transform;
+	}
+
+	protected override CoroutineInstructions Instructions(
+		Transform agent,
+		PluginData data
+	) {
+		return () => this.Move(agent, data);
+	}
+
+	private IEnumerable<YieldInstruction> Move(Transform agent, PluginData data) {
+		Vector3 target = agent.transform.position;
+		do {
+			yield return new WaitForEndOfFrame();
+			target = this.hitter!.TryPoint(agent) ?? target;
+			agent.LookAt(target);
+			this.MoveFrame(agent, data, target);
+		} while (agent.position != target);
+	}
+
+	private void MoveFrame(Transform agent, PluginData data, Vector3 target) {
+		float lerp = this.GetLerp(agent.position, target);
+		agent.position = this.MoveTowards(agent.position, target, lerp);
+		data.weight = this.GetWeight(lerp);
+	}
+
+	private Vector3 MoveTowards(Vector3 current, Vector3 target, float lerp) {
+		float speed = Mathf.Lerp(this.min.speed, this.max.speed, lerp);
+		return Vector3.MoveTowards(current, target, Time.deltaTime * speed);
+	}
+
+	private float GetLerp(Vector3 current, Vector3 target) {
+		return Mathf.InverseLerp(
+			this.min.distance,
+			this.max.distance,
+			(target - current).magnitude
+		);
+	}
+
+	private float GetWeight(float lerp) {
+		return Mathf.Lerp(this.min.weight, this.max.weight, lerp);
+	}
+}
