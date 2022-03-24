7978a5dc28f25af6ecf14393e69932ee23e8525a
codaishin <codaishin@gmail.com>
2022-03-24 13:27:36 +0100

    feat(scripts): RequireComponent with optional includeChildren

--- Assets/Scripts/Build/Tools/Extensions/GameObjectExtensions.cs
+++ Assets/Scripts/Build/Tools/Extensions/GameObjectExtensions.cs
@@ -2,8 +2,22 @@ using UnityEngine;
 
 public static class GameObjectExtensions
 {
-	public static T RequireComponent<T>(this GameObject gameObject) {
+	private static bool TryGetComponentInChild<T>(
+		this GameObject gameObject,
+		out T component
+	) where T : class {
+		component = gameObject.GetComponentInChildren<T>();
+		return component != null;
+	}
+
+	public static T RequireComponent<T>(
+		this GameObject gameObject,
+		bool includeChildren = false
+	) where T : class {
 		if (gameObject.TryGetComponent(out T component)) {
+			return component;
+		}
+		if (includeChildren && gameObject.TryGetComponentInChild(out component)) {
 			return component;
 		}
 		throw new MissingComponentException(
