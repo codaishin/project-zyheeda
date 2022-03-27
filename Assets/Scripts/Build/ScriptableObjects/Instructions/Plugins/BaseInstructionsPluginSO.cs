a80d798cc750fa9f576870b72b0375299d2a0727
codaishin <codaishin@gmail.com>
2022-03-27 14:31:46 +0200

    feat(scripts): add generic BasePlugnInstructionSO

--- Assets/Scripts/Build/ScriptableObjects/Instructions/Plugins/BaseInstructionsPluginSO.cs
+++ Assets/Scripts/Build/ScriptableObjects/Instructions/Plugins/BaseInstructionsPluginSO.cs
@@ -9,14 +9,14 @@ public struct PluginCallbacks
 	public Action? onEnd;
 
 	public static PluginCallbacks operator +(
-		PluginCallbacks a,
-		PluginCallbacks b
+		PluginCallbacks fst,
+		PluginCallbacks snd
 	) {
 		return new PluginCallbacks {
-			onBegin = a.onBegin + b.onBegin,
-			onBeforeYield = a.onBeforeYield + b.onBeforeYield,
-			onAfterYield = a.onAfterYield + b.onAfterYield,
-			onEnd = a.onEnd + b.onEnd,
+			onBegin = fst.onBegin + snd.onBegin,
+			onBeforeYield = fst.onBeforeYield + snd.onBeforeYield,
+			onAfterYield = fst.onAfterYield + snd.onAfterYield,
+			onEnd = fst.onEnd + snd.onEnd,
 		};
 	}
 }
@@ -26,4 +26,26 @@ public abstract class BaseInstructionsPluginSO : ScriptableObject
 	public abstract Func<PluginData, PluginCallbacks> GetCallbacks(
 		GameObject agent
 	);
+}
+
+public abstract class BaseInstructionsPluginSO<TAgent, TPluginData> :
+	BaseInstructionsPluginSO
+{
+	public abstract TAgent GetConcreteAgent(GameObject agent);
+	public abstract TPluginData GetPluginData(PluginData data);
+
+	protected abstract PluginCallbacks GetCallbacks(
+		TAgent agent,
+		TPluginData data
+	);
+
+	public override Func<PluginData, PluginCallbacks> GetCallbacks(
+		GameObject agent
+	) {
+		TAgent concreteAgent = this.GetConcreteAgent(agent);
+		return data => {
+			TPluginData pluginData = this.GetPluginData(data);
+			return this.GetCallbacks(concreteAgent, pluginData);
+		};
+	}
 }
--- Assets/Scripts/Tests/ScriptableObjects/Instructions/Plugins/BasePluginInstructionSOTests.cs
+++ Assets/Scripts/Tests/ScriptableObjects/Instructions/Plugins/BasePluginInstructionSOTests.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections;
 using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
 
 public class BasePluginInstructionSOTests : TestCollection
 {
@@ -37,5 +41,100 @@ public class BasePluginInstructionSOTests : TestCollection
 		c.onEnd?.Invoke();
 
 		Assert.AreEqual("ab", called);
+	}
+
+	class MockPluginData : PluginData { }
+	class MockMB : MonoBehaviour { }
+
+	class MockPluginSO : BaseInstructionsPluginSO<MockMB, MockPluginData>
+	{
+		public Func<GameObject, MockMB> getConcreteAgent =
+			obj => obj.RequireComponent<MockMB>();
+		public Func<PluginData, MockPluginData> getPLuginData =
+			data => data.As<MockPluginData>()!;
+		public Func<MockMB, MockPluginData, PluginCallbacks> getCallbacks =
+			(_, __) => new PluginCallbacks();
+
+		public override MockMB GetConcreteAgent(GameObject agent) =>
+			this.getConcreteAgent(agent);
+		public override MockPluginData GetPluginData(PluginData data) =>
+			this.getPLuginData(data);
+		protected override PluginCallbacks GetCallbacks(
+			MockMB agent,
+			MockPluginData data
+		) => this.getCallbacks(agent, data);
+	}
+
+	[UnityTest]
+	public IEnumerator GetConcreteAgent() {
+		var calledAgent = null as GameObject;
+		var calledMB = null as MonoBehaviour;
+		var plugin = ScriptableObject.CreateInstance<MockPluginSO>();
+		var mockMB = new GameObject().AddComponent<MockMB>();
+		var agent = new GameObject().AddComponent<MockMB>();
+
+		plugin.getConcreteAgent = agent => {
+			calledAgent = agent;
+			return mockMB;
+		};
+		plugin.getCallbacks = (agent, _) => {
+			calledMB = agent;
+			return new PluginCallbacks();
+		};
+
+		yield return new WaitForEndOfFrame();
+
+		var partial = plugin.GetCallbacks(agent.gameObject);
+
+		Assert.AreSame(agent.gameObject, calledAgent);
+
+		partial(new MockPluginData());
+
+		Assert.AreSame(mockMB, calledMB);
+	}
+
+	[UnityTest]
+	public IEnumerator GetPluginData() {
+		var calledPluginData = null as PluginData;
+		var calledMockPluginData = null as MockPluginData;
+		var plugin = ScriptableObject.CreateInstance<MockPluginSO>();
+		var pluginData = new PluginData();
+		var mockPluginData = new MockPluginData();
+		var agent = new GameObject().AddComponent<MockMB>();
+
+
+		plugin.getPLuginData = data => {
+			calledPluginData = data;
+			return mockPluginData;
+		};
+		plugin.getCallbacks = (_, data) => {
+			calledMockPluginData = data;
+			return new PluginCallbacks();
+		};
+
+		yield return new WaitForEndOfFrame();
+
+		_ = plugin.GetCallbacks(agent.gameObject)(pluginData);
+
+		Assert.AreSame(pluginData, calledPluginData);
+		Assert.AreSame(mockPluginData, calledMockPluginData);
+	}
+
+	[UnityTest]
+	public IEnumerator ReturnPluginCallbacks() {
+		var called = 0;
+		var plugin = ScriptableObject.CreateInstance<MockPluginSO>();
+		var agent = new GameObject().AddComponent<MockMB>();
+		var callbacks = new PluginCallbacks { onBegin = () => ++called };
+
+		plugin.getCallbacks = (_, __) => callbacks;
+
+		yield return new WaitForEndOfFrame();
+
+		callbacks = plugin.GetCallbacks(agent.gameObject)(new PluginData());
+
+		callbacks.onBegin!();
+
+		Assert.AreEqual(1, called);
 	}
 }
